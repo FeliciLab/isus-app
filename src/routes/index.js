bb45303f8d26b542020967e1599683e75182286f
Paulo Italo <pauloitalo@hotmail.com.br>
2020-04-30 16:39:39 -0300

    #2 - Adicionando o welcome do componente de rotas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,20 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+
 import AppTab from './appBottomTab';
 import teste from '../pages/Settings/about';
+import Welcome from '../pages/Welcome';
 
 const RootStack = createStackNavigator();
+
 export default function App() {
+
   return (
     <NavigationContainer>
       <RootStack.Navigator headerMode="none">
         <RootStack.Screen name="App" component={AppTab} />
+        <RootStack.Screen name="Welcome" component={Welcome} />
         <RootStack.Screen name="teste" component={teste} />
       </RootStack.Navigator>
     </NavigationContainer>
