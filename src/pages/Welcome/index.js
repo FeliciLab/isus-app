e47ce05ce168ef882097ec4928cac61bc924a156
Paulo Italo <pauloitalo@hotmail.com.br>
2020-04-30 16:40:20 -0300

    #2 - Adicionando o asyncStorage nos componentes

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
-  ImageBackground,
-  View, Text, Image, StatusBar, SafeAreaView
+  ImageBackground, View, Text, Image, StatusBar, SafeAreaView
 } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 import { useNavigation } from '@react-navigation/native';
 import Entypo from 'react-native-vector-icons/Entypo';
 import AppIntroSlider from 'react-native-app-intro-slider';
@@ -12,36 +12,37 @@ import intro3 from '../../assets/images/intro3.png';
 import intro4 from '../../assets/images/intro4.png';
 import intro5 from '../../assets/images/intro5.png';
 import tutorialbackground from '../../assets/backgrounds/tutorialbackground.png';
+
 export default function Welcome() {
   const navigation = useNavigation();
 
   const data = [
     {
-      key: 1,
+      key: 'slide-1',
       title: 'Slogan do App',
       description: 'Texto explicando o objetivo do aplicativo em geral e seu valor pro usuário',
       img: intro1
     },
     {
-      key: 2,
+      key: 'slide-2',
       title: 'SUSi',
       description: 'Texto explicando o que o usuário vai encontrar nessa seção',
       img: intro2
     },
     {
-      key: 3,
+      key: 'slide-3',
       title: 'Minha Saúde',
       description: 'Texto explicando o que o usuário vai encontrar nessa seção',
       img: intro3
     },
     {
-      key: 4,
+      key: 'slide-4',
       title: 'Educação',
       description: 'Texto explicando o que o usuário vai encontrar nessa seção',
       img: intro4
     },
     {
-      key: 5,
+      key: 'slide-5',
       title: 'Pesquisa',
       description: 'Texto explicando o que o usuário vai encontrar nessa seção',
       img: intro5
@@ -73,16 +74,23 @@ export default function Welcome() {
     </View>
   );
 
-  const moveToHome = () => navigation.reset({
-    index: 0,
-    routes: [{ name: 'Home' }]
-  });
+  const moveToHome = async () => {
+    try {
+      await AsyncStorage.setItem('@show-tutorial', 'false');
+      return navigation.reset({
+        index: 0,
+        routes: [{ name: 'App' }]
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   return (
     <>
       <StatusBar translucent backgroundColor="transparent" />
       <SafeAreaView style={{ flex: 1 }}>
-          <ImageBackground source={tutorialbackground} style={{flex: 1, resizeMode: 'cover', justifyContent: 'center'}}>
+          <ImageBackground source={tutorialbackground} style={{ flex: 1, resizeMode: 'cover', justifyContent: 'center' }}>
             <AppIntroSlider
               KeyExtractor={item => item.key}
               renderItem={renderItem}
