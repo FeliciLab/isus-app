24b65726f542e28b7b4969adee1148faabb0e850
Mauricio Holanda <maumauholanda0@gmail.com>
2020-04-30 21:16:31 -0300

    TopTab dinamico

--- src/routes/appTopTab.js
+++ src/routes/appTopTab.js
@@ -1,24 +1,42 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-
 import Education from '../pages/Education';
+import { getCategoriasData } from '../apis/apiHome';
 
 const EducationTab = createMaterialTopTabNavigator();
 export default function EducationTabScreen() {
+  const [categorias, setCategorias] = useState([
+    {
+      categoria: {
+        name: 'Menu',
+        slug: 'Menu',
+        term_group: 0,
+        term_id: 0
+      }
+    }
+  ]);
+
+  useEffect(() => {
+    getCategoriasData().then((response) => {
+      setCategorias(response.data);
+    });
+  }, []);
+
   return (
     <EducationTab.Navigator
       tabBarOptions={{
+        scrollEnabled: true,
         labelStyle: { fontSize: 10 },
         indicatorStyle: { backgroundColor: '#4CAF50' }
       }}
     >
-      <EducationTab.Screen
-        name="Recursos Educativos"
-        component={Education}
-        options={{ headerShown: true }}
-      />
-      <EducationTab.Screen name="Protocolos Institucionais" component={Education} />
-      <EducationTab.Screen name="Notas e Portarias" component={Education} />
+      {categorias.map(item => (
+        <EducationTab.Screen
+          key={item.categoria.term_id}
+          name={item.categoria.name}
+          component={Education}
+        />
+      ))}
     </EducationTab.Navigator>
   );
 }
--- src/services/request.js
+++ src/services/request.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const request = axios.create({
   timeout: 20000,
-  // baseURL: 'https:',
+  baseURL: 'http://165.227.182.4/api/',
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json'
