389104e8009f3cfd3db800ca239a969a29569d83
Paulo Italo <pauloitalo@hotmail.com.br>
2020-05-06 14:35:54 -0300

    reestruturação da tela de Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,8 +6,20 @@ import {
   Title, Card, Caption, Paragraph
 } from 'react-native-paper';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import FontAwesome5Icon from 'react-native-vector-icons/FontAwesome5';
+import antIcon from 'react-native-vector-icons/AntDesign';
 import { useNavigation } from '@react-navigation/native';
 import AsyncStorage from '@react-native-community/async-storage';
+import Educacao from '../../assets/icons/educacao.svg';
+import Pesquisa from '../../assets/icons/pesquisa.svg';
+import Servico1 from '../../assets/icons/servicos/servico_1.svg';
+import Servico2 from '../../assets/icons/servicos/servico_2.svg';
+import Servico3 from '../../assets/icons/servicos/servico_3.svg';
+import Servico4 from '../../assets/icons/servicos/servico_4.svg';
+import Forca4 from '../../assets/icons/forca_4.svg';
+import IconPaciente from '../../assets/icons/icon_paciente.svg';
+
+import normalize from '../../utils/normalize';
 
 export default function HomeScreen() {
   const navigation = useNavigation();
@@ -27,7 +39,7 @@ export default function HomeScreen() {
   redirectToWelcome();
 
   function HomeCard({
-    onPress, icon, iconSize, title, width, height
+    onPress, FontIcon, Logo, logoSize, title, width, height, color
   }) {
     return (
       <Card
@@ -38,18 +50,103 @@ export default function HomeScreen() {
           height: Dimensions.get('window').width / (width || 4.5),
           width: Dimensions.get('window').width / (height || 4.5),
           justifyContent: 'center',
-          borderColor: '#111',
-          borderWidth: 0.2
+          borderColor: color,
+          borderWidth: 1
         }}
         onPress={onPress}
       >
         <View style={{ flex: 1, alignSelf: 'center', justifyContent: 'center' }}>
-          <Icon name={icon} size={iconSize || 40} color="#111" />
+          {
+            typeof Logo === 'string' ? <FontIcon name={Logo} size={logoSize || 40} color={color} /> : <Logo color={color} />
+
+          }
         </View>
         <Caption style={{ textAlign: 'center', fontSize: 11, lineHeight: 10 }}>{title}</Caption>
       </Card>
     );
   }
+
+  const sections = [
+    {
+      id: 1,
+      title: 'Minha Saúde',
+      logo: 'heart',
+      color: '#F2453D',
+      FontIcon: Icon,
+      onPress: () => Linking.openURL('https://integrasus.saude.ce.gov.br')
+    },
+    {
+      id: 2,
+      title: 'Educação',
+      logo: Educacao,
+      color: '#4CAF50',
+      onPress: () => Linking.openURL('https://integrasus.saude.ce.gov.br')
+    },
+    {
+      id: 3,
+      title: 'Pesquisa',
+      logo: Pesquisa,
+      color: '#4054B2',
+      onPress: () => Linking.openURL('https://integrasus.saude.ce.gov.br')
+    }
+  ];
+
+  const services = [
+    {
+      id: 1,
+      title: 'IntegraSUS',
+      logo: Servico1,
+      onPress: () => Linking.openURL('https://integrasus.saude.ce.gov.br')
+    },
+    {
+      id: 2,
+      title: 'Central de Ventiladores',
+      logo: Servico2,
+      onPress: () => Linking.openURL('https://gestao-ventiladores.dev.org.br/')
+    },
+    {
+      id: 3,
+      title: 'TeleMedicina',
+      logo: Servico3,
+      onPress: () => Linking.openURL('https://coronavirus.ceara.gov.br/isus/telemedicina')
+    },
+    {
+      id: 4,
+      title: 'Mapa da saúde',
+      logo: Servico4,
+      onPress: () => Linking.openURL('https://mapas.esp.ce.gov.br')
+    },
+  ];
+
+  const anticoronaActions = [
+    {
+      id: 1,
+      title: 'Boletins',
+      logo: 'bulletin-board',
+      FontIcon: Icon,
+      onPress: () => Linking.openURL('https://coronavirus.ceara.gov.br/isus/boletins/')
+    },
+    {
+      id: 2,
+      title: 'Notificações de casos',
+      logo: 'form',
+      FontIcon: antIcon,
+      onPress: () => Linking.openURL('https://notifica.saude.gov.br/login')
+    },
+    {
+      id: 3,
+      title: 'Farmaco-vigilância',
+      logo: 'pills',
+      FontIcon: FontAwesome5Icon,
+      onPress: () => Linking.openURL('https://coronavirus.ceara.gov.br/isus/farmacovigilancia/')
+    },
+    {
+      id: 4,
+      title: 'Ações do governo',
+      logo: Forca4,
+      onPress: () => Linking.openURL('https://coronavirus.ceara.gov.br/isus/governo/')
+    },
+  ];
 
   return (
     <ScrollView style={{ backgroundColor: '#fff', flex: 1 }}>
@@ -61,34 +158,22 @@ export default function HomeScreen() {
            marginHorizontal: 11
          }}
        >
-        <HomeCard
-          title="Minha Saúde"
-          icon="heart"
-          width={3.3}
-          height={3.3}
-          iconSize={60}
-          onPress={() => Linking.openURL('https://integrasus.saude.ce.gov.br')}
-        />
-        <HomeCard
-          title="Central de Ventiladores"
-          icon="clipboard-plus"
-          width={3.3}
-          height={3.3}
-          iconSize={60}
-          onPress={() => Linking.openURL('https://gestao-ventiladores.dev.org.br/')}
-        />
-
-        <HomeCard
-          title="TeleMedicina"
-          icon="wechat"
-          width={3.3}
-          height={3.3}
-          iconSize={60}
-          onPress={() => Linking.openURL('https://coronavirus.ceara.gov.br/isus/telemedicina')}
-        />
-
+         {
+           sections.map(section => (
+            <HomeCard
+              title={section.title}
+              Logo={section.logo}
+              width={3.3}
+              height={3.3}
+              logoSize={60}
+              FontIcon={section.FontIcon || Icon}
+              color={section.color}
+              onPress={section.onPress}
+            />
+           ))
+         }
        </View>
-      <Title style={{ margin: 15 }}>Serviços</Title>
+      <Title style={{ margin: 15, color: '#FF9800', fontSize: 20 }}>Serviços</Title>
       <View
         style={{
           flexDirection: 'row',
@@ -97,31 +182,21 @@ export default function HomeScreen() {
           marginHorizontal: 11
         }}
       >
-        <HomeCard
-          title="IntegraSUS"
-          icon="heart"
-          iconSize={40}
-          onPress={() => Linking.openURL('https://integrasus.saude.ce.gov.br')}
-        />
-        <HomeCard
-          title="Central de Ventiladores"
-          icon="clipboard-plus"
-          onPress={() => Linking.openURL('https://gestao-ventiladores.dev.org.br/')}
-        />
-
-        <HomeCard
-          title="TeleMedicina"
-          icon="wechat"
-          onPress={() => Linking.openURL('https://coronavirus.ceara.gov.br/isus/telemedicina')}
-        />
-        <HomeCard
-          title="Mapa da saúde"
-          icon="map-search"
-          onPress={() => Linking.openURL('https://mapas.esp.ce.gov.br')}
-        />
+        {
+          services.map(service => (
+            <HomeCard
+              key={service.id}
+              title={service.title}
+              Logo={service.logo}
+              FontIcon={service.FontIcon || Icon}
+              color="#FF9800"
+              onPress={service.onPress}
+            />
+          ))
+        }
       </View>
 
-      <Title style={{ margin: 15 }}>Força-tarefa Anticorona</Title>
+      <Title style={{ margin: 15, fontSize: 20 }}>Força-tarefa Anticorona</Title>
       <View
         style={{
           flexDirection: 'row',
@@ -130,60 +205,51 @@ export default function HomeScreen() {
           marginHorizontal: 16
         }}
       >
-        <HomeCard
-          title="Boletins"
-          icon="newspaper"
-          onPress={() => Linking.openURL('https://coronavirus.ceara.gov.br/isus/boletins/')}
-        />
-        <HomeCard
-          title="Notificações de casos"
-          icon="page-next-outline"
-          onPress={() => Linking.openURL('https://notifica.saude.gov.br/login')}
-        />
-
-        <HomeCard
-          title="Farmaco-vigilância"
-          icon="pill"
-          onPress={() => Linking.openURL('https://coronavirus.ceara.gov.br/isus/farmacovigilancia/')
-          }
-        />
-        <HomeCard
-          title="Açoes do governo"
-          icon="calendar-month"
-          onPress={() => Linking.openURL('https://coronavirus.ceara.gov.br/isus/governo/')}
-        />
+        {
+          anticoronaActions.map(actions => (
+            <HomeCard
+              key={actions.id}
+              title={actions.title}
+              Logo={actions.logo}
+              FontIcon={actions.FontIcon || Icon}
+              color="rgba(0, 0, 0, 0.6)"
+              onPress={actions.onPress}
+            />
+          ))
+        }
       </View>
 
       <Card
         onPress={() => Linking.openURL('https://coronavirus.ceara.gov.br/profissional/manejoclinico/')
         }
         style={{
-          margin: 20,
+          marginVertical: 20,
+          marginHorizontal: 16,
           borderRadius: 10,
           backgroundColor: '#4054B2',
           height: 130,
-          padding: 10
           // alignItems: 'center'
         }}
       >
-        <View style={{ flexDirection: 'row' }}>
+        <View style={{ flexDirection: 'row', alignItems: 'center', flex: 1 }}>
           <View
             style={{
               height: 80,
               width: 80,
-              backgroundColor: '#fff',
               borderRadius: 80,
-              alignSelf: 'center',
-              justifyContent: 'center'
+              marginHorizontal: 24
             }}
           >
-            <Icon style={{ alignSelf: 'center' }} name="doctor" size={40} color="#111" />
+            <IconPaciente />
           </View>
-          <View style={{ marginHorizontal: 15, alignSelf: 'center' }}>
-            <Paragraph style={{ maxWidth: 200, color: '#FFEB3B' }}>
+          <View style={{
+            flex: 2
+          }}
+          >
+            <Paragraph style={{ fontSize: normalize(16), color: '#FFEB3B' }}>
               Manejo Clínico de Paciente com Covid-19
             </Paragraph>
-            <Caption style={{ textAlign: 'center', maxWidth: 200, color: '#F2F2F2' }}>
+            <Caption style={{ color: '#F2F2F2' }}>
               Orientações sobre cada estágio de atendimento a pacientes com Covid-19
             </Caption>
           </View>
