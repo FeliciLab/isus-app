7111bcd375d0b86f73cbdfc8494cea84c7569cf9
Junior-Shyko <franciscoanto@gmail.com>
2020-05-18 14:53:54 -0300

    Folder content

---
+++ src/pages/Content/Description.js
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import { View, Image, Dimensions } from 'react-native';
+import { Title } from 'react-native-paper';
+// import { WebView } from 'react-native-webview';
+import HTML from 'react-native-render-html';
+import { useNavigation } from '@react-navigation/native';
+
+export default function DescriptionScreen(props) {
+  const navigation = useNavigation();
+  const { route } = props;
+  const { item } = route.params;
+
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      headerTintColor: '#FFF',
+      headerStyle: {
+        backgroundColor: '#4CAF50',
+        elevation: 0,
+        shadowOpacity: 0
+      }
+    });
+  });
+
+  return (
+    <View style={{ flex: 1, backgroundColor: '#fff' }}>
+      <Title>{item.post_title}</Title>
+      <Image
+        resizeMode="contain"
+        style={{
+          height: Dimensions.get('window').width / 1.5,
+          width: Dimensions.get('window').width
+        }}
+        source={{ uri: `${item.image}` }}
+      />
+      <View
+        style={{
+          height: Dimensions.get('window').width / 1.5,
+          width: Dimensions.get('window').width
+        }}
+      >
+        <View style={{ padding: 10, alignContent: 'center' }}>
+          <HTML html={item.content} />
+        </View>
+      </View>
+    </View>
+  );
+}
