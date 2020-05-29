836359f4748ef7ae91636c59ee52fa0f579528a3
mbernardes19 <matheus.bernardes@thoughtworks.com>
2020-05-29 17:08:50 -0300

    Adicionamos setup do OneSignal na raiz da aplicacao

--- src/App.js
+++ src/App.js
@@ -1,15 +1,64 @@
+/* eslint-disable import/order */
+/* eslint-disable no-unused-vars */
+/* eslint-disable class-methods-use-this */
 import 'intl';
 import 'intl/locale-data/jsonp/pt-BR';
-// import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'react-native';
-import React from 'react';
+import React, { useEffect } from 'react';
+import OneSignal from 'react-native-onesignal'; // Import package from node modules
 import Routes from './routes';
+import { useNavigation } from '@react-navigation/native';
 
-export default function App() {
+export default function App(properties) {
+  const navigation = useNavigation();
+  useEffect(() => {
+    OneSignal.setLogLevel(6, 0);
+
+    // Replace 'YOUR_ONESIGNAL_APP_ID' with your OneSignal App ID.
+    OneSignal.init('917766a7-c01e-4655-89a1-86f648be2fc8', { kOSSettingsKeyAutoPrompt: false, kOSSettingsKeyInAppLaunchURL: false, kOSSettingsKeyInFocusDisplayOption: 2 });
+    OneSignal.inFocusDisplaying(2);
+
+    OneSignal.promptForPushNotificationsWithUserResponse(myiOSPromptCallback);
+
+    OneSignal.addEventListener('received', onReceived);
+    OneSignal.addEventListener('opened', onOpened);
+    OneSignal.addEventListener('ids', onIds);
+
+    return function cleanup() {
+      OneSignal.removeEventListener('received', onReceived);
+      OneSignal.removeEventListener('opened', onOpened);
+      OneSignal.removeEventListener('ids', onIds);
+    };
+  }, []);
+
+  function onReceived(notification) {
+    console.log('Notification received: ', notification);
+  }
+
+  function onOpened(openResult) {
+    console.log('Launch URL:', openResult.notification.payload.launchURL);
+    console.log('Message: ', openResult.notification.payload.body);
+    console.log('Data: ', openResult.notification.payload.additionalData);
+    console.log('isActive: ', openResult.notification.isAppInFocus);
+    console.log('openResult: ', openResult);
+
+    navigation.navigate('webview', {
+      title: 'Notificação',
+      url: 'https://google.com.br'
+    });
+  }
+
+  function onIds(device) {
+    console.log('Device info: ', device);
+  }
+
   return (
     <>
-      <StatusBar backgroundColor="#4CAF50" />
-      <Routes />
+        <StatusBar backgroundColor="#4CAF50" />
+        <Routes />
     </>
   );
+}
+function myiOSPromptCallback(permission) {
+  // do something with permission value
 }
