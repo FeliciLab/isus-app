02ae291145debfbf244cd2fc632b04a25cc0f4b2
Mauricio Holanda <maumauholanda0@gmail.com>
2020-06-18 09:30:42 -0300

    adicionado codePush android

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import 'intl/locale-data/jsonp/pt-BR';
 import { StatusBar, Platform } from 'react-native';
 import React, { useEffect } from 'react';
 import OneSignal from 'react-native-onesignal';
+import codePush from 'react-native-code-push';
 import Routes from './routes';
 import { navigationRef, navigate } from './routes/rootNavigation';
 
-export default function App() {
+function App() {
   useEffect(() => {
     OneSignal.setLogLevel(6, 0);
 
-    OneSignal.init('917766a7-c01e-4655-89a1-86f648be2fc8', { kOSSettingsKeyAutoPrompt: false, kOSSettingsKeyInAppLaunchURL: true, kOSSettingsKeyInFocusDisplayOption: 2 });
+    OneSignal.init('917766a7-c01e-4655-89a1-86f648be2fc8', {
+      kOSSettingsKeyAutoPrompt: false,
+      kOSSettingsKeyInAppLaunchURL: true,
+      kOSSettingsKeyInFocusDisplayOption: 2
+    });
     OneSignal.inFocusDisplaying(2);
 
     OneSignal.promptForPushNotificationsWithUserResponse(myiOSPromptCallback);
@@ -30,7 +35,10 @@ export default function App() {
 
   function onOpened(openResult) {
     if (Platform.OS !== 'ios' && openResult.notification.payload.launchURL) {
-      return navigate('webview', { title: openResult.notification.payload.title, url: openResult.notification.payload.launchURL });
+      return navigate('webview', {
+        title: openResult.notification.payload.title,
+        url: openResult.notification.payload.launchURL
+      });
     }
     return navigate('App');
   }
@@ -41,8 +49,9 @@ export default function App() {
 
   return (
     <>
-        <StatusBar backgroundColor="#4CAF50" />
-        <Routes navigationRef={navigationRef} />
+      <StatusBar backgroundColor="#4CAF50" />
+      <Routes navigationRef={navigationRef} />
     </>
   );
 }
+export default codePush(App);
