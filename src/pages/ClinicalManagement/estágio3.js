fb273ae82729c0e8ddde000f0c33e3020043b082
Paulo Italo <pauloitalo@hotmail.com.br>
2020-06-19 13:56:34 -0300

    #169 - :recycle: refatoração do estágio 3

--- src/pages/ClinicalManagement/estágio3.js
+++ src/pages/ClinicalManagement/estágio3.js
@@ -7,50 +7,70 @@ import { Paragraph } from 'react-native-paper';
 import checkPlatform from '../../utils/PDF';
 import Internação from './text-content/internacao-hospitalar.json';
 
-const Estágio3 = ({ navigation }) => (
-  <>
-    <View style={{ marginTop: 20 }}>
-      <Paragraph>
-        {Internação.sections.paragraphOne.firstPhrase}
-        <Text style={{ fontWeight: 'bold' }}>
-          {Internação.sections.paragraphOne.bold}
-        </Text>
-        {Internação.sections.paragraphOne.secondPhrase}
-        <Text onPress={() => navigation.navigate('webview', { title: Internação.sections.paragraphOne.link.title, url: Internação.sections.paragraphOne.link.url })} style={{ textDecorationLine: 'underline', color: '#FF9800' }}>{Internação.sections.paragraphOne.link.text}</Text>
-      </Paragraph>
 
-      {Internação.sections.paragraphOne.items.map(item => (
+const Estágio3 = ({ navigation }) => {
+  const tópicos = [
+    tópico1 => (
+      <>
         <Paragraph>
-          {item}
+          {tópico1.texto1}
+          <Text style={{ fontWeight: 'bold' }}>
+            {tópico1.destaque}
+          </Text>
+          {tópico1.texto2}
+          <Text onPress={() => navigation.navigate('webview', { title: tópico1.link.título, url: tópico1.link.url })} style={{ textDecorationLine: 'underline', color: '#FF9800' }}>{tópico1.link.texto}</Text>
         </Paragraph>
-      ))}
+        {tópico1.itens.map(item => (
+            <Paragraph>
+              {item}
+            </Paragraph>
+        ))}
+      </>
+    ),
+    tópico2 => (
       <Paragraph style={{ marginVertical: 8 }}>
-        <Text style={{ fontWeight: 'bold' }}>{Internação.sections.paragraphTwo.bold}</Text>
-        {Internação.sections.paragraphTwo.secondPhrase}
-        <Text onPress={() => (checkPlatform(Internação.sections.paragraphTwo.link.url, 'Restricao do uso do oseltamivir.pdf'))} style={{ textDecorationLine: 'underline', color: '#FF9800' }}>{Internação.sections.paragraphTwo.link.text}</Text>
-        {Internação.sections.paragraphTwo.ThirdPhrase}
+      <Text style={{ fontWeight: 'bold' }}>{tópico2.destaque}</Text>
+      {tópico2.texto1}
+      <Text onPress={() => (checkPlatform(tópico2.link.url, 'Restricao do uso do oseltamivir.pdf'))} style={{ textDecorationLine: 'underline', color: '#FF9800' }}>{tópico2.link.texto}</Text>
+      {tópico2.texto2}
       </Paragraph>
+    ),
+    tópico3 => (
       <Paragraph>
-        <Text style={{ fontWeight: 'bold' }}>{Internação.sections.paragraphThree.bold}</Text>
-        {Internação.sections.paragraphThree.secondPhrase}
-      <Text onPress={() => navigation.navigate('webview', { title: Internação.sections.paragraphThree.link.title, url: Internação.sections.paragraphThree.link.url })} style={{ textDecorationLine: 'underline', color: '#FF9800' }}>{Internação.sections.paragraphThree.link.text}</Text>
+        <Text style={{ fontWeight: 'bold' }}>{tópico3.destaque}</Text>
+        {tópico3.texto1}
+      <Text onPress={() => navigation.navigate('webview', { title: tópico3.link.título, url: tópico3.link.url })} style={{ textDecorationLine: 'underline', color: '#FF9800' }}>{tópico3.link.texto}</Text>
       </Paragraph>
+    ),
+    tópico4 => (
       <Paragraph>
-        <Text style={{ fontWeight: 'bold' }}>{Internação.sections.paragraphFour.bold}</Text>
-        {Internação.sections.paragraphFour.secondPhrase}
-        <Text onPress={() => navigation.navigate('webview', { title: Internação.sections.paragraphFour.link.title, url: Internação.sections.paragraphFour.link.url })} style={{ textDecorationLine: 'underline', color: '#FF9800' }}>{Internação.sections.paragraphFour.link.text}</Text>
+        <Text style={{ fontWeight: 'bold' }}>{tópico4.destaque}</Text>
+        {tópico4.texto1}
+        <Text onPress={() => navigation.navigate('webview', { title: tópico4.link.título, url: tópico4.link.url })} style={{ textDecorationLine: 'underline', color: '#FF9800' }}>{tópico4.link.texto}</Text>
       </Paragraph>
+    ),
+    tópico5 => (
       <Paragraph style={{ marginVertical: 8 }}>
-        {Internação.sections.paragraphFive.firstPhrase}
-        <Text style={{ fontWeight: 'bold' }}>{Internação.sections.paragraphFive.bold}</Text>
-        <Text onPress={() => navigation.navigate('webview', { title: Internação.sections.paragraphFive.link.title, url: Internação.sections.paragraphFive.link.url })} style={{ textDecorationLine: 'underline', color: '#FF9800' }}>{Internação.sections.paragraphFive.link.text}</Text>
+        {tópico5.texto1}
+        <Text style={{ fontWeight: 'bold' }}>{tópico5.destaque}</Text>
+        <Text onPress={() => navigation.navigate('webview', { title: tópico5.link.título, url: tópico5.link.url })} style={{ textDecorationLine: 'underline', color: '#FF9800' }}>{ tópico5.link.text }</Text>
       </Paragraph>
+    ),
+    tópico6 => (
+      <Paragraph>
+        {tópico6.texto}
+      </Paragraph>
+    )
 
-      <Paragraph>
-        {Internação.sections.paragraphSix}
-      </Paragraph>
+  ];
+
+  return (
+    <>
+    <View style={{ marginTop: 20 }}>
+      { tópicos.map((gerarTópico, index) => gerarTópico(Internação.tópicos[index]))}
     </View>
-  </>
-);
+    </>
+  );
+};
 
 export default Estágio3;
