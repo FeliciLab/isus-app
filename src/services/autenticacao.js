62024cda6c932a2eebd9ac63970acb04e5dc004f
Paulo Italo <pauloitalo@hotmail.com.br>
2020-07-16 14:57:20 -0300

    #196 - :sparkles: adiciona servico de autenticacao e manipulacao no storage dos usuarios

--- src/services/autenticacao.js
+++ src/services/autenticacao.js
@@ -1,20 +1,78 @@
 import Login from 'react-native-login-keycloak';
+import { salvarDados, removerDados, pegarDados } from './armazenamento';
 
 const configuracao = {
   url: 'https://dev.id.org.br/auth/',
   realm: 'saude',
-  clientId: 'account',
-  redirectUri: 'https://dev.id.org.br/auth/',
-  appsiteURi: 'isusapp://login/isus',
+  clientId: 'isus',
+  redirectUri: 'isusapp://isusapp/secured/profile',
+  appsiteUri: 'isusapp://',
 };
 
 async function autenticarComIdSaude() {
+  await Login.startLoginProcess(configuracao);
+  salvarUsuarioLogadoNoStorage();
+}
+
+async function fazerLogoutDoIdSaude() {
   try {
-    const tokens = await Login.startLoginProcess(configuracao);
-    console.log(tokens);
+    const deslogou = await Login.logoutKc();
+    if (!deslogou) {
+      await removerDados('react-native-keycloak-tokens');
+    }
+    await removerUsuarioLogado();
   } catch (err) {
     console.log(err);
   }
 }
 
-export default autenticarComIdSaude;
+async function pegarTokensSalvosNoDispositivo() {
+  try {
+    const tokensSalvos = await Login.getTokens();
+    console.log('tokens =>', tokensSalvos);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function pegarDadosDeUsuarioNoIdSaude() {
+  const dadosDoUsuario = await Login.retrieveUserInfo();
+  return dadosDoUsuario;
+}
+
+async function salvarUsuarioLogadoNoStorage() {
+  try {
+    const dadosDoUsuario = await pegarDadosDeUsuarioNoIdSaude();
+    await salvarDados('@usuario-logado', {
+      inscricao: dadosDoUsuario.sub,
+      nome: `${dadosDoUsuario.given_name} ${dadosDoUsuario.family_name}`,
+      email: dadosDoUsuario.email
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function removerUsuarioLogado() {
+  try {
+    await removerDados('@usuario-logado');
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function pegarDadosDeUsuarioNoStorage() {
+  try {
+    const dadosDoUsuario = await pegarDados('@usuario-logado');
+    return dadosDoUsuario;
+  } catch (err) {
+    throw err;
+  }
+}
+
+export {
+  autenticarComIdSaude,
+  pegarDadosDeUsuarioNoStorage,
+  pegarTokensSalvosNoDispositivo,
+  fazerLogoutDoIdSaude
+};
