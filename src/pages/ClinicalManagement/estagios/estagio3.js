853dce6a57d495380f1bc3e2f7b0c552d77728a7
mbernardes19 <bernardes.matheus@outlook.com>
2020-07-28 18:55:45 -0300

    #228 :bug: Corrige bug de carregamento de links do Manejo

--- src/pages/ClinicalManagement/estagios/estagio3.js
+++ src/pages/ClinicalManagement/estagios/estagio3.js
@@ -18,7 +18,7 @@ const Estagio3 = ({ navigation }) => {
             {secao1.destaque}
           </Text>
           {secao1.texto2}
-          <Text onPress={() => navigation.navigate('webview', { title: secao1.link.titulo, url: secao1.link.url })} style={estilo.link}>{secao1.link.texto}</Text>
+          <Text onPress={() => navigation.navigate('manejoWebview', { title: secao1.link.titulo, url: secao1.link.url })} style={estilo.link}>{secao1.link.texto}</Text>
         </Paragraph>
         {secao1.itens.map(item => (
             <Paragraph key={item.id}>
@@ -39,21 +39,21 @@ const Estagio3 = ({ navigation }) => {
       <Paragraph key="3">
         <Text style={estilo.destaque}>{secao3.destaque}</Text>
         {secao3.texto1}
-      <Text onPress={() => navigation.navigate('webview', { title: secao3.link.titulo, url: secao3.link.url })} style={estilo.link}>{secao3.link.texto}</Text>
+      <Text onPress={() => navigation.navigate('manejoWebview', { title: secao3.link.titulo, url: secao3.link.url })} style={estilo.link}>{secao3.link.texto}</Text>
       </Paragraph>
     ),
     secao4 => (
       <Paragraph key="4">
         <Text style={estilo.destaque}>{secao4.destaque}</Text>
         {secao4.texto1}
-        <Text onPress={() => navigation.navigate('webview', { title: secao4.link.titulo, url: secao4.link.url })} style={estilo.link}>{secao4.link.texto}</Text>
+        <Text onPress={() => navigation.navigate('manejoWebview', { title: secao4.link.titulo, url: secao4.link.url })} style={estilo.link}>{secao4.link.texto}</Text>
       </Paragraph>
     ),
     secao5 => (
       <Paragraph key="5" style={estilo.margin8}>
         {secao5.texto1}
         <Text style={estilo.destaque}>{secao5.destaque}</Text>
-        <Text onPress={() => navigation.navigate('webview', { title: secao5.link.titulo, url: secao5.link.url })} style={estilo.link}>{ secao5.link.texto }</Text>
+        <Text onPress={() => navigation.navigate('manejoWebview', { title: secao5.link.titulo, url: secao5.link.url })} style={estilo.link}>{ secao5.link.texto }</Text>
       </Paragraph>
     ),
     secao6 => (
