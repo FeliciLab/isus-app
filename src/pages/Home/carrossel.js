355028457ea6fe92d8cd2a5b70fc2c8f278cdc08
mbernardes19 <bernardes.matheus@outlook.com>
2020-07-29 18:36:57 -0300

    #225 :sparkles: Cria paginação do carrossel

--- src/pages/Home/carrossel.js
+++ src/pages/Home/carrossel.js
@@ -1,12 +1,18 @@
-import * as React from 'react';
-import Carousel from 'react-native-snap-carousel';
+import React, { useState } from 'react';
+import Carousel, { Pagination } from 'react-native-snap-carousel';
 import Banner from './banner';
 
 export default function Carrossel({ sliderWidth, itemWidth }) {
+  const [indiceAtivo, alterarIndiceAtivo] = useState(0);
+
   const carouselItems = [
     {
       title: 'Item 1',
-    }];
+    },
+    {
+      title: 'Item 1',
+    }
+  ];
 
   function cardItem() {
     return (
@@ -14,12 +20,40 @@ export default function Carrossel({ sliderWidth, itemWidth }) {
     );
   }
 
+  function paginacao() {
+    return (
+      <Pagination
+        dotsLength={carouselItems.length}
+        dotColor="#FF9800"
+        inactiveDotColor="#191919"
+        activeDotIndex={indiceAtivo}
+        dotStyle={{
+          height: 12,
+          width: 12,
+          marginHorizontal: -3,
+          borderRadius: 100
+        }}
+        inactiveDotScale={1}
+        containerStyle={{
+          marginTop: -35
+        }}
+        animatedDuration={10}
+        animatedTension={100}
+        delayPressInDot={50}
+      />
+    );
+  }
+
   return (
-    <Carousel
-      data={carouselItems}
-      renderItem={cardItem}
-      sliderWidth={sliderWidth}
-      itemWidth={itemWidth}
-    />
+    <>
+      <Carousel
+        data={carouselItems}
+        renderItem={cardItem}
+        sliderWidth={sliderWidth}
+        itemWidth={itemWidth}
+        onSnapToItem={indice => alterarIndiceAtivo(indice)}
+      />
+      { paginacao() }
+    </>
   );
 }
