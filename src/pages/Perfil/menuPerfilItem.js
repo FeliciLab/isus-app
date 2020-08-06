2c93ed7c82d1353824019d8d412e1a5616e3da05
mbernardes19 <bernardes.matheus@outlook.com>
2020-08-06 13:16:38 -0300

    #240 :recycle: Refatoração de MenuPerfilItem

--- src/pages/Perfil/menuPerfilItem.js
+++ src/pages/Perfil/menuPerfilItem.js
@@ -9,17 +9,27 @@ import { useNavigation } from '@react-navigation/native';
 export default function MenuPerfilItem({ titulo }) {
   const navigation = useNavigation();
 
+  const ItemInterior = () => (
+    <View style={estilos.itemInterior}>
+      <Text style={estilos.titulo}>{titulo}</Text>
+      <IconMaterial style={estilos.iconeDireita} color="rgba(0, 0, 0, 0.6)" name="keyboard-arrow-right" size={25} />
+    </View>
+  );
+
+  const Item = ({ children }) => (
+    <View style={estilos.item}>
+      {children}
+      <Divider style={estilos.divisor} />
+    </View>
+  );
+
   return (
     <TouchableOpacity onPress={() => navigation.navigate('EMCONSTRUCAO')}>
       <View style={estilos.itemContainer}>
-        <Icon style={{ marginRight: 10 }} name="heart" size={25} color="rgba(0, 0, 0, 0.54)" />
-        <View style={{ marginBottom: 15, flexDirection: 'column', width: '100%' }}>
-          <View style={{ flexDirection: 'row', marginBottom: 9, marginTop: 2 }}>
-            <Text style={estilos.titulo}>{titulo}</Text>
-            <IconMaterial style={{ marginLeft: 'auto', paddingRight: 25 }} color="rgba(0, 0, 0, 0.6)" name="keyboard-arrow-right" size={25} />
-          </View>
-          <Divider style={{ backgroundColor: 'rgba(0, 0, 0, 0.22)' }} />
-        </View>
+        <Icon style={estilos.iconeEsquerda} name="heart" size={25} color="rgba(0, 0, 0, 0.54)" />
+        <Item>
+          <ItemInterior />
+        </Item>
       </View>
     </TouchableOpacity>
   );
@@ -33,5 +43,25 @@ const estilos = StyleSheet.create({
   },
   itemContainer: {
     flexDirection: 'row',
+  },
+  item: {
+    marginBottom: 15,
+    flexDirection: 'column',
+    width: '100%'
+  },
+  itemInterior: {
+    flexDirection: 'row',
+    marginBottom: 9,
+    marginTop: 2
+  },
+  iconeEsquerda: {
+    marginRight: 10
+  },
+  iconeDireita: {
+    marginLeft: 'auto',
+    paddingRight: 25
+  },
+  divisor: {
+    backgroundColor: 'rgba(0, 0, 0, 0.22)'
   }
 });
