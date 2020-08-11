1cb491a526d2dd55d64fc676cac342117f82f026
amaurymeireles <amaurymeireles@gmail.com>
2020-08-11 16:43:03 -0300

    #260 - :sparkles: Adiciona contexto de formulario

---
+++ src/context/FormContext.js
@@ -0,0 +1,46 @@
+import React, { createContext } from 'react';
+import { useForm } from 'react-hook-form';
+
+const FormContext = createContext({});
+
+export const FormProvider = ({ initValues, children }) => {
+  const {
+    register,
+    errors,
+    triggerValidation,
+    handleSubmit,
+    setValue,
+    getValues,
+    control,
+  } = useForm({
+    defaultValues: initValues || {},
+    mode: 'onBlur',
+    reValidateMode: 'onChange',
+  });
+
+  const setValues = obj => Object.keys(obj).forEach(key => setValue(key, obj[key]));
+
+  return (
+    <FormContext.Provider
+      value={{
+        register,
+        errors,
+        triggerValidation,
+        handleSubmit,
+        setValue,
+        setValues,
+        getValues,
+        control,
+      }}
+    >
+      {children}
+    </FormContext.Provider>
+  );
+};
+
+FormProvider.defaultProps = {
+  initValues: {},
+};
+
+
+export default FormContext;
