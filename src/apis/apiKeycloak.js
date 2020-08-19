27830dcc0d222a3202bebda00c50a3805b346235
Paulo Italo <pauloitalo@hotmail.com.br>
2020-08-19 14:39:22 -0300

    #243 - 👽  altera a estrtura do valor para a Categoria Profissional no Cadastro do ID Saude

--- src/apis/apiKeycloak.js
+++ src/apis/apiKeycloak.js
@@ -4,6 +4,17 @@ export function autenticar(email, senha) {
   return request.post('auth', { email, senha });
 }
 
+export async function pegarListaDeServicos() {
+  const resultado = await request.get('/unidades-servico');
+  return resultado.data;
+}
+
+export async function pegarListaDeCategoriasProfissionais() {
+  const resultado = await request.get('/categorias-profissionais');
+  return resultado.data;
+}
+
+
 export function logout(token) {
   return request.post('logout', { refresh_token: token.refresh_token }, { headers: { Authorization: `Bearer ${token.access_token}` } });
 }
