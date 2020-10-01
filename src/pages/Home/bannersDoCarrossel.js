714669dd54e9467d2c4594a4b4700b7565c2835d
Paulo Italo <pauloitalo@hotmail.com.br>
2020-10-01 14:06:54 -0300

    #363 - 🔥 retira o banner do PPSUS da Home do App

--- src/pages/Home/bannersDoCarrossel.js
+++ src/pages/Home/bannersDoCarrossel.js
@@ -2,7 +2,7 @@ import React from 'react';
 import BannerManejo from './bannerManejo';
 import Banner from './banner';
 import SUS30anos from '../../assets/images/SUS-30-anos.png';
-import PPSUS from '../../assets/images/PPSUS.png';
+
 import CartilhaSaudeMental from '../../assets/images/cartilha-saude-mental.png';
 
 export default [
@@ -12,14 +12,6 @@ export default [
         titulo="SUS 30 anos"
         imagem={SUS30anos}
         enderecoUrl="https://www.esp.ce.gov.br/tag/semana-do-sus/"
-      />
-  },
-  {
-    banner:
-      <Banner
-        titulo="PPSUS"
-        imagem={PPSUS}
-        enderecoUrl="https://www.esp.ce.gov.br/tag/ppsus/"
       />
   },
   {
