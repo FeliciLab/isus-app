0181a247882557fc5706dcf937e143d0bf6cf3a6
nicoledbianchin <nicoledbianchin@gmail.com>
2020-10-02 15:15:23 -0300

    #314 :triangular_flag_on_post: - remove feature toggle

--- src/featureAtivas.js
+++ src/featureAtivas.js
@@ -1,7 +1,6 @@
 import Feature from './utils/features';
 
 export default [
-  Feature.PERFIL,
   Feature.DISPOR_NOSSOS_SERVICOS_EM_CARROSSEL,
   Feature.ADICIONAR_INFORMACOES_PROFISSIONAIS_NAO_CADASTRADAS,
   Feature.SECAO_CONTEUDO_HOME_PERSONALIZADA,
