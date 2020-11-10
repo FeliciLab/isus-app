cbe5edc45be410e0f8312ab10a2a0405877eb1ec
nicoledbianchin <nicoledbianchin@gmail.com>
2020-11-10 16:22:35 -0300

    #323 :clown_face: - cria dados do usuario mockado

---
+++ __mocks__/valores/dadosUsuarioMock.js
@@ -0,0 +1,30 @@
+export default {
+  id: 47,
+  id_keycloak: '5fe15ff1-9f8e-470f-a718-c3fde3b29013',
+  name: 'Nome ',
+  email: 'Restates@asd.com',
+  cpf: '74534550065',
+  telefone: '30492304923',
+  created_at: '2020-11-04T17:30:20.000000Z',
+  updated_at: '2020-11-09T16:59:06.000000Z',
+  municipio: {
+    id: 1126,
+    estado_id: 6,
+    nome: 'Acopiara'
+  },
+  estado: {
+    id: 6,
+    nome: 'Ceará',
+    uf: 'CE'
+  },
+  profissional: {
+    categoria_profissional: {
+      id: 3,
+      nome: 'Enfermagem'
+    },
+    tipos_contratacoes: [],
+    titulacoes_academica: [],
+    unidades_servicos: [],
+    especialidades: []
+  }
+};
