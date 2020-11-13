bbb8d1a7ff8cd334bef1291bdf0981fa86b7d107
Paulo Italo Rodrigues <pauloitalo@hotmail.com.br>
2020-11-13 14:57:18 -0300

    #323 - 🔧  atualiza configurações de jest com coverage e ajustes de dependências

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,24 @@
 module.exports = {
   preset: 'react-native',
   collectCoverage: true,
+  setupFiles: [
+    './node_modules/react-native-gesture-handler/jestSetup.js'
+  ],
+  moduleNameMapper: {
+    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+    '\\.(css|less)$': 'identity-obj-proxy'
+  },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   transformIgnorePatterns: [
     'node_modules/(?!(react-native'
     + '|react-navigation-tabs'
     + '|react-native-splash-screen'
     + '|react-native-screens'
+    + '|react-native-gesture-handler'
+    + '|react-native-iphone-x-helper'
+    + '|react-native-material-dropdown-v2'
+    + '|react-native-material-ripple'
+    + '|react-native-material-buttons'
     + '|react-native-vector-icons'
     + '|react-native-config'
     + '|rn-fetch-blob'
