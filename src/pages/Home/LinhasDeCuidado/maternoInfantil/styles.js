7ed9d52da1dfa10e1491b045dd4f1aa7ab490e64
Rui Moreno <ruiguemo@gmail.com>
2020-12-03 23:02:04 -0300

    #393 - :bug: bug do banner corrigido

--- src/pages/Home/LinhasDeCuidado/maternoInfantil/styles.js
+++ src/pages/Home/LinhasDeCuidado/maternoInfantil/styles.js
@@ -1,14 +1,31 @@
 import styled from 'styled-components/native';
-import { Text } from 'react-native';
 
-const StyledTexto = styled(Text)`
-    font-size: 14;
-    color: rgba(0, 0, 0, 0.6)
+const Container = styled.View`
+  background-color: #fff;
 `;
 
-const StyledTitulo = styled(Text)`
-    font-size: 18;
-    color: rgba(0, 0, 0, 1)
+const ScrollView = styled.ScrollView`
+  background-color: #fff;
 `;
 
-export { StyledTexto, StyledTitulo };
+const SvgView = styled.View`
+  margin-top: 24px;
+`;
+
+const Titulo = styled.Text`
+  font-size: 18px;
+  color: rgba(0, 0, 0, 1);  
+`;
+
+const Texto = styled.Text`
+  font-size: 14px;
+  color: rgba(0, 0, 0, 0.6);
+`;
+
+export {
+  Container,
+  ScrollView,
+  SvgView,
+  Titulo,
+  Texto
+};
