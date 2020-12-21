553147999d109b2b9c55aa43f3d8e4f44c0ea287
Paulo <pauloitalo@hotmail.com.br>
2020-12-21 16:37:50 -0300

    #324 💬 - altera o texto da tela de sucesso da edição de dados Profissionais

--- src/constantes/textos.js
+++ src/constantes/textos.js
@@ -2,13 +2,13 @@ const PERFIL = {
   EDICAO_INFO_PESSOAIS: {
     CABEÇALHO: 'Informações Pessoais',
     INTRODUCAO: 'Vamos realizar seu cadastro, '
-              + 'precisamos apenas de algumas informações:',
+      + 'precisamos apenas de algumas informações:',
     TITULO: 'Informações Pessoais:',
     MSG_EMAIL: 'O email deve ser no formato exemplo@exemplo.com',
     MSG_EMAIL_EXISTE: 'Email já cadastrado',
     MSG_TELEFONE: 'O telefone deve ter pelo menos 11 número',
-    MSG_SUCESSO: 'Parabéns! Você cadastrou suas informações profissionais. '
-               + 'Você será redirecionado para sua página de Perfil.'
+    MSG_SUCESSO: 'Parabéns! Você atualizou suas informações pessoais. '
+      + 'Você será redirecionado para sua página de Perfil.'
   }
 };
 
