ff1786c5d6f7bd3b1a0f07671ec4e0494b4ec204
Paulo <pauloitalo@hotmail.com.br>
2021-01-14 14:48:32 -0300

    #357 - 📝  adiciona documentação da função analyticsData

--- src/utils/analytics.js
+++ src/utils/analytics.js
@@ -1,5 +1,11 @@
 import analytics from '@react-native-firebase/analytics';
 
+/**
+ * Função para registrar evento no Google Analytics
+ * @param {string} name É a label que irá aparecer no analytics.
+ * @param {string} event Corresponde à ação que gerou esse evento (Click, swipe, scroll...)
+ * @param {*} category É a categoria na qual o evento será inserido.
+ */
 const analyticsData = async (
   name, event, category
 ) => {
