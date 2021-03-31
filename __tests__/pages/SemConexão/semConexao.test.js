abf4f1dbb9fb69c66a575e0120b22a798301fbdf
Rui Moreno <ruiguemo@gmail.com>
2021-03-31 10:41:26 -0300

    #384 - ✅ Atualização semConexao.test

--- __tests__/pages/SemConexão/semConexao.test.js
+++ __tests__/pages/SemConexão/semConexao.test.js
@@ -4,6 +4,10 @@ import { render, fireEvent } from 'util-teste';
 import { TESTIDS } from '../../../src/constantes/testIDs';
 import SemConexao from '../../../src/pages/SemConexao';
 
+const mockLinking = jest.fn(() => Promise.resolve('500'));
+jest.mock('react-native/Libraries/Linking/Linking', () => ({
+  openURL: mockLinking
+}));
 
 jest.mock('@react-native-community/netinfo', () => ({
   ...jest.requireActual('@react-native-community/netinfo'),
@@ -12,57 +16,56 @@ jest.mock('@react-native-community/netinfo', () => ({
   })
 }));
 
-
-const mockedNavigate = jest.fn();
+const mockNavigation = jest.fn();
 
 jest.mock('@react-navigation/native', () => ({
   ...jest.requireActual('@react-navigation/native'),
   useNavigation: () => ({
-    navigate: mockedNavigate,
-    setOptions: mockedNavigate,
+    navigate: mockNavigation,
+    setOptions: mockNavigation,
   }),
   useIsFocused: jest.fn()
 }));
 
 describe('testes na tela de sem conexão webviews', () => {
-  // test(`renderizar ${TESTIDS.SEM_CONEXAO.BOTAO_VOLTAR}`, () => {
-  //   const params = {
-  //     componente: 'webview',
-  //     title: '',
-  //     url: '',
-  //     rota: '',
-  //     expanded: true
-  //   };
-  //   const { getByTestId } = render(
-  //     <SemConexao {...{ route: { params } }} />
-  //   );
-  //   const BotaoVoltar = getByTestId(TESTIDS.SEM_CONEXAO.BOTAO_VOLTAR);
+  test(`renderizar ${TESTIDS.SEM_CONEXAO.BOTAO_VOLTAR}`, () => {
+    const params = {
+      componente: 'webview',
+      title: '',
+      url: '',
+      rota: '',
+      expanded: true
+    };
+    const { getByTestId } = render(
+      <SemConexao {...{ route: { params } }} />
+    );
+    const BotaoVoltar = getByTestId(TESTIDS.SEM_CONEXAO.BOTAO_VOLTAR);
 
-  //   fireEvent.press(BotaoVoltar);
-  //   expect(BotaoVoltar).not.toBeNull();
-  //   expect(mockedNavigate).toHaveBeenCalled();
+    fireEvent.press(BotaoVoltar);
+    expect(BotaoVoltar).not.toBeNull();
+    expect(mockNavigation).toHaveBeenCalled();
 
-  //   const iconeLaranja = getByTestId(TESTIDS.SEM_CONEXAO.ICONE_SEM_CONEXAO_LARANJA);
-  //   expect(iconeLaranja).not.toBeNull();
-  // });
+    const iconeLaranja = getByTestId(TESTIDS.SEM_CONEXAO.ICONE_SEM_CONEXAO_LARANJA);
+    expect(iconeLaranja).not.toBeNull();
+  });
 
-  // test(`renderizar ${TESTIDS.SEM_CONEXAO.BOTAO_TENTAR_NOVAMENTE}`, () => {
-  //   const params = {
-  //     componente: 'webview',
-  //     title: '',
-  //     url: '',
-  //     rota: '',
-  //     expanded: true
-  //   };
-  //   const { getByTestId } = render(<SemConexao {...{ route: { params } }} />);
-  //   const BotaoTentarNovamente = getByTestId(
-  //     TESTIDS.SEM_CONEXAO.BOTAO_TENTAR_NOVAMENTE
-  //   );
+  test(`renderizar ${TESTIDS.SEM_CONEXAO.BOTAO_TENTAR_NOVAMENTE}`, () => {
+    const params = {
+      componente: 'webview',
+      title: '',
+      url: '',
+      rota: '',
+      expanded: true
+    };
+    const { getByTestId } = render(<SemConexao {...{ route: { params } }} />);
+    const BotaoTentarNovamente = getByTestId(
+      TESTIDS.SEM_CONEXAO.BOTAO_TENTAR_NOVAMENTE
+    );
 
-  //   fireEvent.press(BotaoTentarNovamente);
-  //   expect(BotaoTentarNovamente).not.toBeNull();
-  //   expect(mockedNavigate).toHaveBeenCalled();
-  // });
+    fireEvent.press(BotaoTentarNovamente);
+    expect(BotaoTentarNovamente).not.toBeNull();
+    expect(mockNavigation).toHaveBeenCalled();
+  });
 
   test(`renderizar ${TESTIDS.SEM_CONEXAO.BOTAO_VOLTAR} da tela falha de conexão`, () => {
     const params = {
@@ -81,18 +84,19 @@ describe('testes na tela de sem conexão webviews', () => {
     fireEvent.press(BotaoTentarNovamente);
     fireEvent.press(BotaoTentarNovamente);
     expect(BotaoTentarNovamente).not.toBeNull();
-    expect(mockedNavigate).toHaveBeenCalled();
+    expect(mockNavigation).toHaveBeenCalled();
 
     const iconeVermelho = getByTestId(TESTIDS.SEM_CONEXAO.ICONE_SEM_CONEXAO_VERMELHO);
     expect(iconeVermelho).not.toBeNull();
   });
 });
+
 describe('testes na tela de sem conexão browser', () => {
-  test('renderizar tela ESP Virtual ', () => {
+  test('renderizar tela ESP Virtual com e sem parametros', () => {
     const params = {
       componente: 'browser',
       title: '',
-      url: '',
+      url: 'http://espvirtual.esp.ce.gov.br/',
       rota: '',
       expanded: true
     };
@@ -101,5 +105,7 @@ describe('testes na tela de sem conexão browser', () => {
     const BotaoTentarNovamente = getByTestId(TESTIDS.SEM_CONEXAO.BOTAO_TENTAR_NOVAMENTE);
     fireEvent.press(BotaoTentarNovamente);
     expect(BotaoTentarNovamente).not.toBeNull();
+    expect(mockLinking).toHaveBeenCalled();
+    expect(mockLinking).toHaveBeenCalledWith(params.url);
   });
 });
