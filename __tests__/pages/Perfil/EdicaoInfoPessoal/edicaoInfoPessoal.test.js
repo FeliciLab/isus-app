0e83002259e0b9e8e54db81af71ea5b30851efb9
Rui Moreno <ruiguemo@gmail.com>
2021-05-20 11:49:46 -0300

    #489 - ✅🔧 Atualiza testes e configurações

--- __tests__/pages/Perfil/EdicaoInfoPessoal/edicaoInfoPessoal.test.js
+++ __tests__/pages/Perfil/EdicaoInfoPessoal/edicaoInfoPessoal.test.js
@@ -1,44 +1,34 @@
-// import React, { useContext } from 'react';
+import React from 'react';
 // eslint-disable-next-line import/no-unresolved
-// import { render } from 'util-teste';
-// import TextInputMask from '../../../../__mocks__/react-native-text-input-mask';
-// import { FormContext, FormProvider } from '../../../../src/context/FormContext';
-// import EdicaoInfoPessoal from '../../../../src/pages/Perfil/EdicaoInfoPessoal';
-
-jest.mock('react-native-text-input-mask', () => ({
-  mask: jest.fn()
-}));
+import { render } from 'util-teste';
+import { FormProvider } from '../../../../src/context/FormContext';
+import EdicaoInfoPessoal from '../../../../src/pages/Perfil/EdicaoInfoPessoal';
 
 const mockNavigation = jest.fn();
 jest.mock('@react-navigation/native', () => ({
   ...jest.requireActual('@react-navigation/native'),
   useNavigation: () => ({
-    navigate: mockNavigation
+    navigate: mockNavigation,
+    setOptions: mockNavigation
   }),
   useIsFocused: jest.fn()
 }));
 
-// const mockTextInputMask = jest.fn();
-// jest.mock('react-native-text-input-mask', () => ({
-//   mask: mockTextInputMask,
-//   default: mockTextInputMask
-// }));
-
 describe('Dado que estou na tela de edição de informações pessoais', () => {
-  // let compTextoTopo;
-  // const textoTopo = 'Edite as informações pessoais que você deseja atualizar';
+  let compTextoTopo;
+  const textoTopo = 'Edite as informações pessoais que você deseja atualizar:';
 
   beforeEach(() => {
-    // const { getByTestId } = render(
-    //   <FormProvider>
-    //     <EdicaoInfoPessoal />
-    //   </FormProvider>
-    // );
-    // compTextoTopo = getByTestId('texto');
+    const { getByTestId } = render(
+      <FormProvider>
+        <EdicaoInfoPessoal />
+      </FormProvider>
+    );
+    compTextoTopo = getByTestId('texto');
   });
   describe('Quando a tela é renderizada corretamente', () => {
     test('Então devo visualizar o texto no topo', () => {
-      // expect(compTextoTopo).toEqual(textoTopo);
+      expect(compTextoTopo.props.children).toEqual(textoTopo);
     });
   });
 });
