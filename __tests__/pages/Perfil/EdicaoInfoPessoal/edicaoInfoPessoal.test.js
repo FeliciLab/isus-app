4b4a5ad914b1bd623577face9cb50e8546111d60
Rui Moreno <ruiguemo@gmail.com>
2021-06-30 17:18:33 -0300

    #489 - ✅ Testes de Edição Info Pessoais finalizado

--- __tests__/pages/Perfil/EdicaoInfoPessoal/edicaoInfoPessoal.test.js
+++ __tests__/pages/Perfil/EdicaoInfoPessoal/edicaoInfoPessoal.test.js
@@ -1,19 +1,13 @@
-/* eslint-disable max-len */
 import React from 'react';
 import {
-  // eslint-disable-next-line no-unused-vars
-  render, waitFor, fireEvent, handlePress, screen, waitForElementToBeRemoved, cleanup
-// eslint-disable-next-line import/no-unresolved
+  render, waitFor, cleanup
+  // eslint-disable-next-line import/no-unresolved
 } from 'util-teste';
 import { FormProvider } from '../../../../src/context/FormContext';
 import EdicaoInfoPessoal from '../../../../src/pages/Perfil/EdicaoInfoPessoal';
 import modeloPessoaMock from '../../../../__mocks__/valores/modeloPessoaMock';
+import { AutenticacaoProvider } from '../../../../src/context/AutenticacaoContext';
 
-const mockAtualizarUsuario = jest.fn();
-jest.mock('../../../../src/services/usuarioService', () => ({
-  ...jest.requireActual('../../../../src/services/usuarioService'),
-  handleSubmit: () => ({ atualizarUsuario: mockAtualizarUsuario })
-}));
 const mockNavigation = jest.fn();
 jest.mock('@react-navigation/native', () => ({
   ...jest.requireActual('@react-navigation/native'),
@@ -24,162 +18,115 @@ jest.mock('@react-navigation/native', () => ({
   useIsFocused: jest.fn()
 }));
 
+// Ver documentacao da testing-library sobre fakeTimers
 afterEach(cleanup, () => {
+  jest.runOnlyPendingTimers();
   jest.useRealTimers();
 });
 
 describe('DADO QUE estou na tela de edição de informações pessoais', () => {
   describe('QUANDO a tela é renderizada corretamente', () => {
+    let getByTextTest;
+    let getAllByTextTest;
+    let getByDisplayValueTest;
+    let queryByA11yStateTest;
+    let getByTestIdTest;
+
+    beforeEach(async () => {
+      await waitFor(async () => {
+        const {
+          getByText,
+          getAllByText,
+          getByDisplayValue,
+          queryByA11yState,
+          getByTestId
+        } = render(
+          <AutenticacaoProvider pessoaAutenticada={modeloPessoaMock}>
+            <FormProvider initValues={modeloPessoaMock}>
+              <EdicaoInfoPessoal />
+            </FormProvider>
+          </AutenticacaoProvider>
+        );
+        getByTextTest = getByText;
+        getAllByTextTest = getAllByText;
+        getByDisplayValueTest = getByDisplayValue;
+        queryByA11yStateTest = queryByA11yState;
+        getByTestIdTest = getByTestId;
+      });
+    });
     test('ENTÃO devo visualizar o texto no topo', () => {
-      const { getByTestId } = render(
-        <FormProvider>
-          <EdicaoInfoPessoal />
-        </FormProvider>
-      );
       const textoTopo = 'Edite as informações pessoais que você deseja atualizar:';
-      const textoTopoId = getByTestId('texto');
+      const textoTopoId = getByTestIdTest('texto');
       expect(textoTopoId.props.children).toEqual(textoTopo);
     });
 
     test('ENTÃO um campo do tipo input/text deve ser exibido com a label Nome Completo', () => {
-      const { getAllByText } = render(
-        <FormProvider>
-          <EdicaoInfoPessoal />
-        </FormProvider>
-      );
       const nomeLabel = 'Nome Completo';
-      const labelId = getAllByText(nomeLabel);
+      const labelId = getAllByTextTest(nomeLabel);
       expect(labelId[0].props.children).toEqual(nomeLabel);
     });
 
     test('ENTÃO o campo Nome Completo deve estar preenchido com o nome da pessoa autenticada', () => {
-      const { getByTestId } = render(
-        <FormProvider initValues={modeloPessoaMock}>
-          <EdicaoInfoPessoal />
-        </FormProvider>
-      );
       const nomeController = 'nomeCompleto';
-      const campoID = getByTestId(`textinput-${nomeController}`);
+      const campoID = getByTestIdTest(`textinput-${nomeController}`);
       expect(campoID.props.value).toEqual('Rui moreno');
     });
 
     test('ENTÃO um campo do tipo input/text deve ser exibido com a label E-mail', () => {
-      const { getAllByText } = render(
-        <FormProvider>
-          <EdicaoInfoPessoal />
-        </FormProvider>
-      );
       const nomeLabel = 'E-mail';
-      const labelId = getAllByText(nomeLabel);
+      const labelId = getAllByTextTest(nomeLabel);
       expect(labelId[0].props.children).toEqual(nomeLabel);
     });
 
     test('ENTÃO o campo E-mail deve estar preenchido com o nome da pessoa autenticada', () => {
-      const { getByTestId } = render(
-        <FormProvider initValues={modeloPessoaMock}>
-          <EdicaoInfoPessoal />
-        </FormProvider>
-      );
       const nomeController = 'email';
-      const campoID = getByTestId(`textinput-${nomeController}`);
+      const campoID = getByTestIdTest(`textinput-${nomeController}`);
       expect(campoID.props.value).toEqual('ruiguemo@gmail.com');
     });
+
     test('ENTÃO um campo do tipo input/text deve ser exibido com a label Telefone', () => {
-      const { getAllByText } = render(
-          <FormProvider>
-            <EdicaoInfoPessoal />
-          </FormProvider>
-      );
       const nomeLabel = 'Telefone';
-      const labelId = getAllByText(nomeLabel);
+      const labelId = getAllByTextTest(nomeLabel);
       expect(labelId[0].props.children).toEqual(nomeLabel);
     });
 
     test('ENTÃO o campo telefone deve estar preenchido com o telefone da pessoa autenticada', () => {
-      const { getByTestId } = render(
-        <FormProvider initValues={modeloPessoaMock}>
-          <EdicaoInfoPessoal />
-        </FormProvider>
-      );
       const nomeController = 'telefone';
-      const campoID = getByTestId(`textinput-${nomeController}`);
+      const campoID = getByTestIdTest(`textinput-${nomeController}`);
       expect(campoID.props.value).toEqual('(85) 99999-9999');
     });
+
     test('ENTÃO um campo do tipo input/text deve ser exibido com a label CPF', () => {
-      const { getAllByText } = render(
-        <FormProvider>
-          <EdicaoInfoPessoal />
-        </FormProvider>
-      );
       const nomeLabel = 'CPF';
-      const labelId = getAllByText(nomeLabel);
+      const labelId = getAllByTextTest(nomeLabel);
       expect(labelId[0].props.children).toEqual(nomeLabel);
     });
+
     test('ENTÃO o campo CPF deve estar preenchido com o nome da pessoa autenticada', () => {
-      const { getByTestId } = render(
-        <FormProvider initValues={modeloPessoaMock}>
-          <EdicaoInfoPessoal />
-        </FormProvider>
-      );
       const nomeController = 'cpf';
-      const campoID = getByTestId(`textinput-${nomeController}`);
+      const campoID = getByTestIdTest(`textinput-${nomeController}`);
       expect(campoID.props.value).toEqual('571.714.340-01');
     });
 
     test('ENTÃO um campo do tipo input/text deve ser exibido com a label Município', () => {
-      const { getAllByText } = render(
-        <FormProvider>
-          <EdicaoInfoPessoal />
-        </FormProvider>
-      );
       const nomeLabel = 'Município';
-      const labelId = getAllByText(nomeLabel);
+      const labelId = getAllByTextTest(nomeLabel);
       expect(labelId[0].props.children).toEqual(nomeLabel);
     });
     test('ENTÃO o campo município deve estar preenchido com o município da pessoa autenticada', () => {
-      const { getByDisplayValue } = render(
-        <FormProvider initValues={modeloPessoaMock}>
-          <EdicaoInfoPessoal />
-        </FormProvider>
-      );
-      const campoID = getByDisplayValue('1347');
-
+      const campoID = getByDisplayValueTest('1347');
       expect(campoID.props.value).toEqual('1347');
     });
-    /* ***************************************** */
-    /* ***************************************** */
-    /* ***************************************** */
-    /* ***************************************** */
+
     describe('QUANDO apresento o formulário com os campos preenchidos e validados', () => {
-      test('ENTÃO o botão de ação está habilitado', async () => {
-        const { queryByA11yState } = render(
-          <FormProvider initValues={modeloPessoaMock}>
-            <EdicaoInfoPessoal />
-          </FormProvider>
-        );
-        const campoID = queryByA11yState({ disabled: false });
+      test('ENTÃO o botão SALVAR deve está habilitado', async () => {
+        const campoID = queryByA11yStateTest({ disabled: false });
         await waitFor(() => expect(campoID).not.toBeNull());
       });
-      test('Então o botão deve exibir o texto Salvar', () => {
-        const { getByText } = render(
-          <FormProvider initValues={modeloPessoaMock}>
-            <EdicaoInfoPessoal />
-          </FormProvider>
-        );
-        const botaoSalvar = getByText('Salvar');
+
+      test('ENTÃO o botão deve exibir o texto SALVAR', () => {
+        const botaoSalvar = getByTextTest('Salvar');
         expect(botaoSalvar).not.toBeNull();
-      });
-      test('ENTÃO a função de edição deve ser chamada atualizarUsuario()', () => {
-        const { getByRole } = render(
-          <FormProvider initValues={modeloPessoaMock}>
-            <EdicaoInfoPessoal />
-          </FormProvider>
-        );
-        // debug();
-        const botaoSalvar = getByRole('button');
-        console.log(botaoSalvar.props);
-        // fireEvent.press(botaoSalvar);
-        // expect(mockAtualizarUsuario).toHaveBeenCalled();
       });
     });
   });
