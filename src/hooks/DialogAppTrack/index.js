a4d5dbea4d2c0b2a9bfcf21eba28c359e877280a
LAYSA CUNHA <laysacunha@gmail.com>
2021-08-12 16:42:02 -0300

    ✨ #533 - adiciona hook de caixa de diálogo que informa App Track desabilitado

---
+++ src/hooks/DialogAppTrack/index.js
@@ -0,0 +1,31 @@
+import { useContext } from 'react';
+import { CaixaDialogoContext } from '../../context/CaixaDialogoContext';
+import { AppTrackTransparencyContext } from '../../context/AppTrackTransparencyContext';
+
+const useDialogAppTrack = () => {
+  const {
+    mostrarCaixaDialogo,
+    fecharCaixaDialogo,
+  } = useContext(CaixaDialogoContext);
+
+  const {
+    exibirDialogAlertaRastreio
+  } = useContext(AppTrackTransparencyContext);
+
+  const exibirDialog = (tipo) => {
+    if (!exibirDialogAlertaRastreio) {
+      return false;
+    }
+    mostrarCaixaDialogo({
+      titulo: 'App Tracking Transparency',
+      texto: `Para efetuar o ${tipo}, você deve habilitar nas configurações do seu dispositivo a permissão do App Track`,
+      textoConclusao: 'OK',
+      aoConcluir: () => { fecharCaixaDialogo(); }
+    });
+    return true;
+  };
+
+  return { exibirDialog };
+};
+
+export default useDialogAppTrack;
