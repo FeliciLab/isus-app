a6d68df942f1eee1aec4fac27ddb42fe2aae35f0
Chicão Thiago <fthiagogv@gmail.com>
2021-08-30 14:58:30 -0300

    ✨ Usando componentes FormPessoal no PreCadastro

--- src/pages/PreCadastro/PreCadastroProfissional.js
+++ src/pages/PreCadastro/PreCadastroProfissional.js
@@ -1,51 +1,25 @@
 import React, { useContext } from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { Controller } from 'react-hook-form';
 import { ScrollView } from 'react-native-gesture-handler';
 import {
   ContainerBody,
-  ContainerForm,
   RowButton,
-  RowInput,
-  Title
 } from './styles';
 import { BotaoLaranja } from '../../components/Botoes/BotoesCirculares';
 import ROTAS from '../../constantes/rotas';
-import InputCategoria from './InputCategoria';
-import InputEspecialidades from './InputEspecialidades';
-import InputSetores from './InputSetores';
 import FormContext from '../../context/FormContext';
 import { atualizarUsuario } from '../../services/usuarioService';
+import FormProfissional from '../../components/FormPessoa/FormProfissional';
 
 const PreCadastroProfissional = () => {
   const navigator = useNavigation();
-  const { control, getValues } = useContext(FormContext);
+  const { getValues } = useContext(FormContext);
 
   return (
     <>
       <ContainerBody>
         <ScrollView>
-          <ContainerForm>
-            <Title>Cadastro Profissional</Title>
-            <RowInput>
-              <InputCategoria />
-            </RowInput>
-            <RowInput>
-              <Controller
-                control={control}
-                name="_hidden.categoriaProfissional"
-                defaultValue=""
-                render={({ value }) => (
-                  <>
-                    <InputEspecialidades categoria={value} />
-                  </>
-                )}
-              />
-            </RowInput>
-            <RowInput>
-              <InputSetores />
-            </RowInput>
-          </ContainerForm>
+          <FormProfissional hiddenActionButton />
           <RowButton>
             <BotaoLaranja
               onPress={async () => {
