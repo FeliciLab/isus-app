ceed1e4ebe5294cbe715b1ed790410a825d4af60
JefersonNsoares <34108133+JefersonNSoares@users.noreply.github.com>
2021-11-03 17:01:08 -0300

    ✨ 267 - Criado tela Politica de Privacidade

--- src/pages/Perfil/PoliticaDePrivacidade/index.js
+++ src/pages/Perfil/PoliticaDePrivacidade/index.js
@@ -1,7 +1,9 @@
 import React, { useLayoutEffect } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, ScrollView, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import politicaPrivacidade from './politica-privacidade.json';
+import { uniqueId } from 'lodash';
 
 export default function PoliticaDePrivacidadeScreen() {
   const navigation = useNavigation();
@@ -44,8 +46,48 @@ export default function PoliticaDePrivacidadeScreen() {
   });
 
   return (
-    <View>
-      <Text>Política de Privacidade</Text>
-    </View>
+    <ScrollView style={{ backgroundColor: '#fff' }}>
+      <View>
+        <Text style={estilos.titulo}>POLÍTICA DE PRIVACIDADE</Text>
+      </View>
+      <View style={estilos.conteudoTexto}>
+        {politicaPrivacidade.paragrafos.map(paragrafo => (
+          <Text key={uniqueId('paragrafo')} style={estilos.texto}>
+            {paragrafo}
+          </Text>
+        ))}
+        {politicaPrivacidade.secoes.map(secao => (
+          <View key={uniqueId('secao')}>
+            <Text
+              style={{
+                fontWeight: 'bold',
+                fontSize: 18,
+                textAlign: 'center'
+              }}
+            >
+              {secao.titulo}
+            </Text>
+            {secao.paragrafos.map(paragrafo => (
+              <Text key={uniqueId('paragrafo')} style={estilos.texto}>
+                {paragrafo}
+              </Text>
+            ))}
+          </View>
+        ))}
+      </View>
+    </ScrollView>
   );
 }
+
+const estilos = StyleSheet.create({
+  titulo: { fontSize: 23, marginTop: 30, textAlign: 'center' },
+  conteudoTexto: { marginHorizontal: 16, marginBottom: 50 },
+  texto: {
+    color: '#000000',
+    opacity: 0.6,
+    fontSize: 14,
+    marginBottom: 8,
+    marginTop: 8,
+    textAlign: 'justify'
+  }
+});
