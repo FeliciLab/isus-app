d26fed1ed359c8f7523ed0833cf6ffbb2cf28db7
ericsonmoreira <moreira.ericson@gmail.com>
2021-11-04 17:00:48 -0300

    🚧 trabalho em progresso

--- src/pages/Buscar/index.js
+++ src/pages/Buscar/index.js
@@ -1,34 +1,37 @@
-import React, { useState } from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react';
 import { FlatList } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { pegarBusca } from '../../apis/apiHome';
 import useAnalytics from '../../hooks/Analytics';
-
+import useDebounce from '../../hooks/useDebounce';
 import InfoPreview from './InfoPreview';
-import LegendaPesquisando from './LegendaPesquisando';
 import ItemConteudo from './ItemConteudo';
-import LegendaNaoEncontrada from './LegendaNaoEncontrada';
 import RodapeBusca from './RodapeBusca';
-
 import {
   TextSearch,
   TouchableLeft,
   TouchableRight,
-  ViewColumn,
+  ViewColumn
 } from './styles';
 
-const Buscar = (props) => {
+const Buscar = props => {
+  const { navigation } = props;
+
   const { analyticsData } = useAnalytics();
 
   const [loading, setLoading] = useState(false);
 
   const [data, setData] = useState([]);
-  // const [page, setPage] = useState(1);
 
-  const [busca, setBusca] = useState({ atual: '', antiga: '' });
-  const [nadaEncontrado, setNadaEncontrado] = useState(false);
+  const [page, setPage] = useState(1);
 
-  const { navigation } = props;
+  const [lastPage, setLastPage] = useState(0);
+
+  const [termoBusca, setTermoBusca] = useState('');
+
+  const termoBuscaDebounced = useDebounce(termoBusca, 500);
+
   navigation.setOptions({
     headerTintColor: '#FFF',
     headerStyle: {
@@ -36,44 +39,51 @@ const Buscar = (props) => {
       elevation: 0,
       shadowOpacity: 0
     },
-
     headerTitle: () => (
       <TextSearch
         autoFocus
         placeholder="Buscar"
         placeholderTextColor="#FFFFFF"
-        value={busca.atual}
-        onChangeText={value => executarBusca(value)}
+        value={termoBusca}
+        onChangeText={value => setTermoBusca(value)}
       />
     ),
-
     headerRight: () => (
-      <TouchableRight mode="contained" onPress={() => loadRepositories()}>
+      <TouchableRight mode="contained" onPress={() => loadProjetos()}>
         <></>
       </TouchableRight>
     ),
-
     headerLeft: () => (
-      <TouchableLeft onPress={() => { navigation.goBack(); }}>
+      <TouchableLeft
+        onPress={() => {
+          navigation.goBack();
+        }}
+      >
         <Icon name="arrow-left" size={28} color="#FFF" />
       </TouchableLeft>
     )
   });
 
-  const loadRepositories = async () => {
-    await analyticsData('Home', 'Pesquisa', busca.atual);
-    try {
-      const response = await pegarBusca(busca.atual, 1);
-      if (response.data.data.length === 0) {
-        setLoading(false);
-        setNadaEncontrado(true);
-        setData([]);
-        return;
-      }
+  useEffect(() => {
+    if (termoBuscaDebounced !== '') {
+      loadProjetos();
+    } else {
+      setData([]);
+    }
+  }, [termoBuscaDebounced]);
 
-      setNadaEncontrado(false);
-      setData([...data, ...response.data.data]);
-      // setPage((old) => old + 1);
+  const loadProjetos = async () => {
+    await analyticsData('Home', 'Pesquisa', termoBuscaDebounced);
+    try {
+      setLoading(true);
+
+      const {
+        data: { data, last_page }
+      } = await pegarBusca(termoBuscaDebounced, page);
+
+      setData(old => [...old, ...data]);
+
+      setLastPage(last_page);
     } catch (e) {
       console.log('Falha ao buscar', e);
     } finally {
@@ -81,31 +91,59 @@ const Buscar = (props) => {
     }
   };
 
-  const executarBusca = (txt) => {
-    setLoading(true);
-    setBusca({
-      atual: txt,
-      antiga: busca.antiga !== txt ? txt : busca.antiga
-    });
-    loadRepositories();
+  // Load more
+  // const onEndReached = () => {
+  //   console.log('onEndReached');
+  //   setPage(old => old + 1);
+  // };
+
+  const renderListFooter = () => {
+    if (!loading) return null;
+
+    return <RodapeBusca />;
+  };
+
+  const renderListEmpty = () => {
+    if (termoBusca === '') return null;
+
+    return <InfoPreview />;
   };
 
   return (
     <ViewColumn>
-      {busca.atual.length === 0 && <InfoPreview />}
-      {busca.atual.length > 0 && loading && <LegendaPesquisando palavra={busca.atual} />}
-      {busca.atual.length > 0 && !loading && (
+      {/* {termoBusca === '' && <InfoPreview />} */}
+      {/* {termoBusca.length === 0 && <InfoPreview />}
+      {termoBusca.length > 0 && loading && (
+        <LegendaPesquisando palavra={termoBusca} />
+      )}
+      {termoBusca.length > 0 && !loading && (
         <FlatList
           contentContainerStyle={{ flexGrow: 1 }}
           data={data}
-          renderItem={({ item }) => <ItemConteudo item={item} navigation={navigation} />}
+          renderItem={({ item }) => (
+            <ItemConteudo item={item} navigation={navigation} />
+          )}
           keyExtractor={item => item.id}
-          onEndReached={loadRepositories}
+          onEndReached={loadProjetos}
           onEndReachedThreshold={0.2}
           ListFooterComponent={(!loading && <RodapeBusca />) || <></>}
           ListEmptyComponent={nadaEncontrado && <LegendaNaoEncontrada />}
         />
-      )}
+      )} */}
+
+      <FlatList
+        contentContainerStyle={{ flexGrow: 1 }}
+        data={data}
+        renderItem={({ item }) => (
+          <ItemConteudo item={item} navigation={navigation} />
+        )}
+        keyExtractor={item => String(item.id)}
+        // onEndReached={onEndReached}
+        onEndReachedThreshold={0.2}
+        ListEmptyComponent={renderListEmpty}
+        ListFooterComponent={renderListFooter}
+      />
+
     </ViewColumn>
   );
 };
