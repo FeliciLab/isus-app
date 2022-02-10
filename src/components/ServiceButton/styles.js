f4d29b854cde4c8086fabc2e1e3ef781cbd25732
ericsonmoreira <moreira.ericson@gmail.com>
2022-02-10 15:25:25 -0300

    💄 ajuste no layout ServiceButton

---
+++ src/components/ServiceButton/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components/native';
+import { Card, Paragraph } from 'react-native-paper';
+
+export const Container = styled(Card)`
+  margin: 10px;
+`;
+
+export const CardTitle = styled(Paragraph)`
+  font-size: 12px;
+  letter-spacing: 0.25px;
+  line-height: 16px;
+  margin-top: 10px;
+  max-width: 112px;
+`;
+
+export const IconeWrapper = styled.View`
+  overflow: hidden;
+  border-top-left-radius: 8px;
+  border-top-right-radius: 8px;
+  overflow: hidden;
+`;
