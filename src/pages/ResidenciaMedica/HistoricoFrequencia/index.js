5ca924551e72b3696b407f3e8c37f018fb5e8f92
ericsonmoreira <moreira.ericson@gmail.com>
2022-02-21 16:07:22 -0300

    ✨ add HistoricoFrequencia

---
+++ src/pages/ResidenciaMedica/HistoricoFrequencia/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const HistoricoFrequencia = () => {
+  return <View />;
+};
+
+export default HistoricoFrequencia;
