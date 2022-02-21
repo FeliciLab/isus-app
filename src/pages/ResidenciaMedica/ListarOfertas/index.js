4152dc19ea5cb80fb3a091f2f4f18af6491d54e4
ericsonmoreira <moreira.ericson@gmail.com>
2022-02-21 10:38:35 -0300

    🚧 add tela de listar ofertas

---
+++ src/pages/ResidenciaMedica/ListarOfertas/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { Container } from './styles';
+
+const ListarOfertas = () => {
+  return (
+    <Container>
+      <Text>Listar Ofertas</Text>
+    </Container>
+  );
+};
+
+export default ListarOfertas;
