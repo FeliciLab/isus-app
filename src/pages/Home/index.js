5bce96fda84a50962e3ae11fa39ecf353dcde710
ericsonmoreira <moreira.ericson@gmail.com>
2022-04-11 16:34:25 -0300

    ⏪ voltando a Home para o que era antes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,8 @@
-import { yupResolver } from '@hookform/resolvers/yup';
 import { useNavigation } from '@react-navigation/native';
 import React, { useEffect, useLayoutEffect } from 'react';
-import { useForm } from 'react-hook-form';
 import { Dimensions, ScrollView, TouchableOpacity } from 'react-native';
-import { Button } from 'react-native-paper';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import * as yup from 'yup';
 import BarraDeStatus from '~/components/barraDeStatus';
-import ControlledMultipleSelectAccordion from '~/components/ControlledMultipleSelectAccordion';
-import ControlledMultipleSelectModal from '~/components/ControlledMultipleSelectModal';
-import ControlledSelectAccordion from '~/components/ControlledSelectAccordion';
-import ControlledSelectModal from '~/components/ControlledSelectModal';
 import { CORES } from '~/constantes/estiloBase';
 import rotas from '~/constantes/rotas';
 import useAnalytics from '~/hooks/useAnalytics';
@@ -23,64 +15,12 @@ import UserInfo from './UserInfo';
 // import LinhasDeCuidado from './LinhasDeCuidado';
 // import MeusConteudos from './MeusConteudos';
 
-const items = [
-  { value: '0', label: 'Fortaleza' },
-  { value: '1', label: 'Mossoró' },
-  { value: '2', label: 'Sobral' },
-  {
-    value: '3',
-    label:
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis tempore laboriosam iste sapiente, neque provident ipsum ex ipsa, odio quo tempora nisi voluptas explicabo rem nesciunt soluta sit recusandae praesentium',
-  },
-  {
-    value: '4',
-    label: 'Lorem ipsum dolor, sit amet consectetur',
-  },
-  {
-    value: '5',
-    label: 'Lorem ipsum',
-  },
-  {
-    value: '6',
-    label: 'Lorem ipsum dolor',
-  },
-  {
-    value: '7',
-    label: 'Lorem',
-  },
-  {
-    value: '8',
-    label: 'Lorem ip',
-  },
-];
-
-const schema = yup.object({
-  selectedMunicipioId: yup.string().required('Campo obrigatório'),
-  mutipleSelectMunicipioId: yup
-    .array()
-    .min(1, 'Preencher pelo menos um campo.'),
-  selectModalMunicipioId: yup.string().required('Campo obrigatório'),
-  mutipleSelectModalMunicipioId: yup
-    .array()
-    .min(1, 'Preencher pelo menos um campo.'),
-});
-
 export default function Home() {
   const navigation = useNavigation();
 
   const { analyticsData } = useAnalytics();
 
   const { user, showTutorial } = useAutenticacao();
-
-  const { control, handleSubmit } = useForm({
-    defaultValues: {
-      selectedMunicipioId: undefined,
-      mutipleSelectMunicipioId: [],
-      selectModalMunicipioId: undefined,
-      mutipleSelectModalMunicipioId: [],
-    },
-    resolver: yupResolver(schema),
-  });
 
   async function redirectToWelcome() {
     if (showTutorial) {
@@ -150,44 +90,6 @@ export default function Home() {
       />
       <UserInfo />
       <ScrollView style={{ backgroundColor: CORES.BRANCO, flex: 1 }}>
-        <ControlledSelectAccordion
-          control={control}
-          name="selectedMunicipioId"
-          items={items}
-          title="Controlled Select Accordion"
-          placeholder="Município"
-        />
-        <ControlledMultipleSelectAccordion
-          control={control}
-          name="mutipleSelectMunicipioId"
-          items={items}
-          title="Controlled Multiple Select Accordion"
-          placeholder="Cidades"
-        />
-
-        <ControlledSelectModal
-          control={control}
-          name="selectModalMunicipioId"
-          title="Controled Select Modal"
-          items={items}
-          placeholder="Município"
-        />
-
-        <ControlledMultipleSelectModal
-          control={control}
-          name="mutipleSelectModalMunicipioId"
-          title="Controled Multiple Select Modal"
-          items={items}
-          placeholder="Município"
-        />
-
-        <Button
-          icon="camera"
-          mode="contained"
-          onPress={handleSubmit(data => console.log(data))}
-          style={{ marginTop: 12 }}>
-          Confirmar
-        </Button>
         <Banners sliderWidth={width} itemWidth={width} />
         <Servicos navigation={navigation} />
         {/* {estaLogado && <MeusConteudos />} */}
