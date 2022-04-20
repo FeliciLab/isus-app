a2cb382b7685e5204f9137f2c4da3ac4b2932b02
ericsonmoreira <moreira.ericson@gmail.com>
2022-04-20 15:06:23 -0300

    ✨ add useCategoriasProfissionais hook

---
+++ src/hooks/useCategoriasProfissionais.js
@@ -0,0 +1,31 @@
+import { useCallback, useState } from 'react';
+import { pegarListaDeCategoriasProfissionais } from '~/apis/apiKeycloak';
+
+export function useCategoriasProfissionais() {
+  const [categoriasProfissionais, setCategoriasProfissionais] = useState([]);
+
+  const [error, setError] = useState(false);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const featchCategoriasProfissionais = useCallback(async () => {
+    try {
+      setIsLoading(true);
+      const data = await pegarListaDeCategoriasProfissionais();
+      setCategoriasProfissionais(data);
+      setError(false);
+    } catch (e) {
+      setError(e);
+      console.log(`Erro ao listar Categorias Profissionais. ${e}`);
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
+
+  return {
+    categoriasProfissionais,
+    error,
+    isLoading,
+    featchCategoriasProfissionais,
+  };
+}
