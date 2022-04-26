fa72d5d361574b38696979d68e4ff26106a6c0a6
ericsonmoreira <moreira.ericson@gmail.com>
2022-04-26 16:44:36 -0300

    🔥 removendo comentários

--- src/pages/Perfil/EdicaoInfoProfissional/index.js
+++ src/pages/Perfil/EdicaoInfoProfissional/index.js
@@ -3,41 +3,45 @@ import { filter, find } from 'lodash';
 import React, { useEffect, useLayoutEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { atualizarUsuarioApi } from '~/apis/apiCadastro';
+import Alerta from '~/components/alerta';
 import BarraDeStatus from '~/components/barraDeStatus';
 import ControlledMultipleSelectModal from '~/components/ControlledMultipleSelectModal/index';
 import ControlledSelectModal from '~/components/ControlledSelectModal/index';
 import { cabecalhoVoltar } from '~/components/layoutEffect/cabecalhoLayout';
+import { CORES } from '~/constantes/estiloBase';
+import rotas from '~/constantes/rotas';
 import { PERFIL } from '~/constantes/textos';
 import useAutenticacao from '~/hooks/useAutenticacao';
 import { useCategoriasProfissionais } from '~/hooks/useCategoriasProfissionais';
 import { useEspecialidades } from '~/hooks/useEspecialidades';
 import { useServicos } from '~/hooks/useServicos';
 import { BotaoSalvar, Container, TituloPrincipal } from './styles';
-// import Alerta from '~/components/alerta';
-// import FormProfissional from '~/components/FormPessoa/FormProfissional';
-// import { CORES } from '~/constantes/estiloBase';
-// import FormContext from '~/context/FormContext';
-// import ROTAS from '~/constantes/rotas';
-// import { atualizarUsuario } from '~/services/usuarioService';
-// import {
-//   analyticsCategoria,
-//   analyticsUnidadeServico,
-// } from '~/utils/funcoesAnalytics';
 
 function EdicaoInfoProfissional() {
   const navigation = useNavigation();
+
+  const [isOpenAlert, setIsOpenAlert] = useState(false);
+
+  const [alertMessage, setAlertMessage] = useState('');
 
   const { user, updateUser } = useAutenticacao();
 
   const [isLoading, setIsLoading] = useState(false);
 
-  console.log('EdicaoInfoProfissional: user', user);
-
-  const { control, handleSubmit, watch, setValue } = useForm({
+  const {
+    control,
+    handleSubmit,
+    watch,
+    setValue,
+    formState: { isDirty },
+  } = useForm({
     defaultValues: {
-      categoriaProfissionalSelectedId: '',
-      especialidadesSelectedsIds: [],
-      servicosSelectedsIds: [],
+      categoriaProfissionalSelectedId:
+        String(user.categoriaProfissional.id) || '',
+      especialidadesSelectedsIds:
+        user.especialidades.map(item => String(item.id)) || [],
+      servicosSelectedsIds:
+        user.unidadesServicos.map(item => String(item.id)) || [],
     },
   });
 
@@ -54,42 +58,20 @@ function EdicaoInfoProfissional() {
 
   const { especialidades, featchEspecialidades } = useEspecialidades();
 
-  useLayoutEffect(() => {
-    cabecalhoVoltar({
-      navegador: navigation,
-      titulo: PERFIL.EDICAO_INFO_PROFISSIONAL.CABECALHO,
-      cor: 'brancoPreto',
-    });
-  }, []);
-
   useEffect(() => {
     featchServicos();
     featchCategoriasProfissionais();
+    featchEspecialidades(user.categoriaProfissional.id);
   }, []);
 
   useEffect(() => {
-    setValue('especialidadesSelectedsIds', []);
-    featchEspecialidades(categoriaProfissionalSelectedIdWatch);
+    if (isDirty) {
+      setValue('especialidadesSelectedsIds', []);
+      featchEspecialidades(categoriaProfissionalSelectedIdWatch);
+    }
   }, [categoriaProfissionalSelectedIdWatch]);
 
-  useEffect(() => {
-    setValue(
-      'categoriaProfissionalSelectedId',
-      String(user.categoriaProfissional.id),
-    );
-    setValue(
-      'especialidadesSelectedsIds',
-      user.especialidades.map(esp => String(esp.id)),
-    );
-    setValue(
-      'servicosSelectedsIds',
-      user.unidadesServicos.map(esp => String(esp.id)),
-    );
-  }, []);
-
   const handleOnPressNextButton = async dataForm => {
-    // console.log(JSON.stringify(dataForm, null, 2));
-
     try {
       setIsLoading(true);
 
@@ -115,60 +97,47 @@ function EdicaoInfoProfissional() {
         termos: true,
       });
 
-      await updateUser();
+      await updateUser(); // atualiza as informações do usuário internamente no app
 
-      console.log(JSON.stringify(user, null, 2));
+      // TODO: colocar aqui o analytics
+      // estava chamando os métodos analyticsCategoria e analyticsUnidadeServico
+      // mas como essa chamada era antes depois de redirecionar para a tela de sucesso
+      // o erro não era percebido
+      // Rever como enviar essas informações para o analytics
+
+      navigation.navigate('TelaDeSucesso', {
+        textoApresentacao: PERFIL.EDICAO_INFO_PESSOAIS.MSG_SUCESSO,
+        telaDeRedirecionamento: rotas.PERFIL,
+        telaDeBackground: CORES.VERDE,
+      });
     } catch (error) {
       console.log(error);
+      handleShowAlert(PERFIL.EDICAO_INFO_PROFISSIONAL.MSG_ERRO_SALVAR);
     } finally {
       setIsLoading(false);
     }
-
-    // try {
-    //   const result = await atualizarUsuario(
-    //     {
-    //       ...pessoa,
-    //       ...getValues(),
-    //     },
-    //     { somenteProfissionais: true },
-    //   );
-    //   if (result) {
-    //     navigation.navigate('TelaDeSucesso', {
-    //       textoApresentacao: PERFIL.EDICAO_INFO_PESSOAIS.MSG_SUCESSO,
-    //       telaDeRedirecionamento: ROTAS.PERFIL,
-    //       telaDeBackground: CORES.VERDE,
-    //     });
-    //     const categoriaProfissional = JSON.stringify(
-    //       result.categoriaProfissional,
-    //     );
-    //     const uniServ = result.unidadeServico;
-    //     analyticsCategoria(
-    //       categoriaProfissional,
-    //       now,
-    //       'Atualização Cadastro',
-    //     );
-    //     analyticsUnidadeServico(uniServ, now, 'Atualização Cadastro');
-    //   }
-    // } catch (e) {
-    //   console.log(e);
-    //   mostrarAlerta(PERFIL.EDICAO_INFO_PROFISSIONAL.MSG_ERRO_SALVAR);
-    // }
   };
 
-  // const mostrarAlerta = mensagem => {
-  //   setExibicaoDoAlerta(true);
-  //   setMensagemDoAlerta(mensagem);
-  // };
+  const handleShowAlert = mensagem => {
+    setIsOpenAlert(true);
+    setAlertMessage(mensagem);
+  };
+
+  useLayoutEffect(() => {
+    cabecalhoVoltar({
+      navegador: navigation,
+      titulo: PERFIL.EDICAO_INFO_PROFISSIONAL.CABECALHO,
+      cor: 'brancoPreto',
+    });
+  }, []);
 
   return (
     <Container>
       <BarraDeStatus backgroundColor="#ffffff" barStyle="dark-content" />
-
       <TituloPrincipal>
         Vamos agora adicionar suas informações profissionais, para isso,
         selecione as opções abaixo
       </TituloPrincipal>
-
       <ControlledSelectModal
         control={control}
         name="categoriaProfissionalSelectedId"
@@ -212,46 +181,12 @@ function EdicaoInfoProfissional() {
         mode="contained">
         Salvar
       </BotaoSalvar>
-      {/* <FormProfissional
-        labelButton="Salvar"
-        actionPress={handleSubmit(async () => {
-          try {
-            const result = await atualizarUsuario(
-              {
-                ...pessoa,
-                ...getValues(),
-              },
-              { somenteProfissionais: true },
-            );
-            if (result) {
-              navigation.navigate('TelaDeSucesso', {
-                textoApresentacao: PERFIL.EDICAO_INFO_PESSOAIS.MSG_SUCESSO,
-                telaDeRedirecionamento: ROTAS.PERFIL,
-                telaDeBackground: CORES.VERDE,
-              });
-              const categoriaProfissional = JSON.stringify(
-                result.categoriaProfissional,
-              );
-              const uniServ = result.unidadeServico;
-              analyticsCategoria(
-                categoriaProfissional,
-                now,
-                'Atualização Cadastro',
-              );
-              analyticsUnidadeServico(uniServ, now, 'Atualização Cadastro');
-            }
-          } catch (e) {
-            console.log(e);
-            mostrarAlerta(PERFIL.EDICAO_INFO_PROFISSIONAL.MSG_ERRO_SALVAR);
-          }
-        })}
-      /> */}
-      {/* <Alerta
-        visivel={exibicaoDoAlerta}
-        textoDoAlerta={mensagemDoAlerta}
+      <Alerta
+        visivel={isOpenAlert}
+        textoDoAlerta={alertMessage}
         duration={4000}
-        onDismiss={() => setExibicaoDoAlerta(false)}
-      /> */}
+        onDismiss={() => setIsOpenAlert(false)}
+      />
     </Container>
   );
 }
