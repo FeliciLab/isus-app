33bed322dc79252f70c4074cc5d7e4360b222b04
fpontef <sobralense@gmail.com>
2022-04-27 16:39:33 -0300

    💄 update EdicaoInfoPessoal styles

--- src/pages/Perfil/EdicaoInfoPessoal/styles.js
+++ src/pages/Perfil/EdicaoInfoPessoal/styles.js
@@ -1,14 +1,18 @@
 import styled from 'styled-components/native';
 import { List, Button, TextInput } from 'react-native-paper';
 import IconDropdown from 'react-native-vector-icons/MaterialIcons';
+import { Paragraph } from 'react-native-paper';
+import { CORES } from '~/constantes/estiloBase';
 
-export const SafeArea = styled.SafeAreaView`
+// removido o styled.SafeAreaView pois já tem um SafeAreaView ativo
+export const Container = styled.View`
   height: 100%;
   background-color: #ffffff;
 `;
 
 export const Scroll = styled.ScrollView`
   padding: 0 16px 0 16px;
+  margin-bottom: 20px;
 `;
 
 export const ConteudoFormulario = styled.View`
@@ -68,4 +72,30 @@ export const IconeDropdown = styled(IconDropdown)`
 export const TextoDeErro = styled.Text`
   color: red;
   margin-top: 0;
+`;
+
+export const Title = styled(Paragraph)`
+  font-weight: 500;
+  font-size: 20px;
+  margin-bottom: 10px;
+`;
+
+export const ContainerBody = styled.View`
+  flex: 1;
+  justify-content: space-between;
+  background-color: ${CORES.BRANCO};
+`;
+
+export const ContainerForm = styled.View`
+  margin-top: 15px;
+`;
+
+export const RowInput = styled.View`
+  margin-top: 14px;
+  margin-bottom: 14px;
+`;
+
+export const RowButton = styled.View`
+  align-self: flex-end;
+  margin-bottom: 20px;
 `;
