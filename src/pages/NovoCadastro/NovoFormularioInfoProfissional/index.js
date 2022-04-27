ce7e41030358ce73cf77fbe29f2e2e30e098f795
ericsonmoreira <moreira.ericson@gmail.com>
2022-04-27 10:06:35 -0300

    🔥 removendo comentários

--- src/pages/NovoCadastro/NovoFormularioInfoProfissional/index.js
+++ src/pages/NovoCadastro/NovoFormularioInfoProfissional/index.js
@@ -2,19 +2,14 @@ import { useRoute } from '@react-navigation/native';
 import React, { useEffect, useLayoutEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { TouchableOpacity } from 'react-native';
-// import { alteraDadosDoUsuario } from '~/apis/apiCadastro';
-// import Alerta from '~/components/alerta';
 import BarraDeStatus from '~/components/barraDeStatus';
 import ControlledMultipleSelectModal from '~/components/ControlledMultipleSelectModal/index';
 import ControlledSelectModal from '~/components/ControlledSelectModal/index';
-// import DropDown from '~/components/dropdown';
 import rotas from '~/constantes/rotas';
 import { useCategoriasProfissionais } from '~/hooks/useCategoriasProfissionais';
 import { useEspecialidades } from '~/hooks/useEspecialidades';
 import { useServicos } from '~/hooks/useServicos';
 import { ArrowLeftIcon } from '~/icons';
-// import { pegarDados } from '~/services/armazenamento';
-// import { Botao, Scroll, Titulo } from './styles';
 import { Botao, Container, Titulo, SubTitulo } from './styles';
 import { find, filter } from 'lodash';
 
@@ -29,8 +24,6 @@ function NovoFormularioInfoProfissional({ navigation }) {
       especialidadesSelectedsIds: [],
       servicosSelectedsIds: [],
     },
-    // TODO: colocar as validações
-    // resolver: yupResolver(schema),
   });
 
   const categoriaProfissionalSelectedIdWatch = watch(
@@ -147,24 +140,10 @@ function NovoFormularioInfoProfissional({ navigation }) {
         cor={definirCorDosElementos()}
         disabled={false}
         labelStyle={{ color: '#fff' }}
-        // onPress={() => {
-        //   registrarUnidadesDeServico();
-        //   registrarUnidadesDeEspecialidades();
-        //   if (veioDoPerfil) {
-        //     return adicionarInformaçõesProfissionais();
-        //   }
-        //   return alterarTelaDoCadastro();
-        // }}
         onPress={handleSubmit(handleOnPressNextButton)}
         mode="contained">
         {veioDoPerfil ? 'salvar' : 'Próximo'}
       </Botao>
-      {/* <Alerta
-        visivel={exibicaoDoAlerta}
-        // textoDoAlerta={}
-        duration={4000}
-        onDismiss={() => setExibicaoDoAlerta(false)}
-      /> */}
     </Container>
   );
 }
