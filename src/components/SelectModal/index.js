afeae3d948c807cc43c69d60ece943249d2ee75c
Escola de Saúde Pública do Ceará <espce@MacBook-Air-de-Escola.local>
2022-05-02 11:10:07 -0300

    ajustes para ios

--- src/components/SelectModal/index.js
+++ src/components/SelectModal/index.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import {
   FlatList,
   Modal,
+  SafeAreaView,
   StyleSheet,
   Text,
   TouchableOpacity,
@@ -35,30 +36,33 @@ const SelectModal = props => {
         animationType="slide"
         visible={open}
         onRequestClose={() => setOpen(false)}>
-        <View style={styles.header}>
-          <TouchableOpacity onPress={() => setOpen(false)}>
-            <ArrowLeftIcon ArrowLeftIcon size={22} color={CORES.VERDE} />
-          </TouchableOpacity>
-          <Text style={styles.modalHeaderText}>{title}</Text>
-        </View>
-        <FlatList
-          data={items}
-          keyExtractor={item => item.value.toString()}
-          renderItem={({ item }) => (
-            <List.Item
-              title={item.label}
-              onPress={() => handleOnPressItem(item)}
-              right={props =>
-                item.value === value ? (
-                  <List.Icon {...props} icon="check" color={CORES.VERDE} />
-                ) : (
-                  <List.Icon {...props} color={CORES.VERDE} />
-                )
-              }
-            />
-          )}
-          ItemSeparatorComponent={Divider}
-        />
+        <SafeAreaView>
+
+          <View style={styles.header}>
+            <TouchableOpacity onPress={() => setOpen(false)}>
+              <ArrowLeftIcon ArrowLeftIcon size={22} color={CORES.VERDE} />
+            </TouchableOpacity>
+            <Text style={styles.modalHeaderText}>{title}</Text>
+          </View>
+          <FlatList
+            data={items}
+            keyExtractor={item => item.value.toString()}
+            renderItem={({ item }) => (
+              <List.Item
+                title={item.label}
+                onPress={() => handleOnPressItem(item)}
+                right={props =>
+                  item.value === value ? (
+                    <List.Icon {...props} icon="check" color={CORES.VERDE} />
+                  ) : (
+                    <List.Icon {...props} color={CORES.VERDE} />
+                  )
+                }
+              />
+            )}
+            ItemSeparatorComponent={Divider}
+          />
+        </SafeAreaView>
       </Modal>
     </List.Section>
   );
