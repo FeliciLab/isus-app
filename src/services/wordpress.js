e880e029b3cee19d9a49cf5b8da502b656ba69b2
ericsonmoreira <moreira.ericson@gmail.com>
2022-05-03 08:37:52 -0300

    🚧 colocando comentários para remover arquivos [3]

--- src/services/wordpress.js
+++ src/services/wordpress.js
@@ -1,5 +1,6 @@
 import { Config } from 'react-native-config';
 
+// TODO: esse arquivo não deveria estar aqui. Deveria ser um obj de axios.
 export default {
   urlPostagem: id =>
     `${Config.API_URL.replace(
