37570cf300299e1868222bbc5df5431e2defde1c
ericsonmoreira <moreira.ericson@gmail.com>
2022-05-04 11:00:09 -0300

    💄 ajuste na cor do texto

--- src/components/ValidationFieldIndicator/index.js
+++ src/components/ValidationFieldIndicator/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { ActivityIndicator, Text } from 'react-native';
-
+import { CORES } from '~/constantes/estiloBase';
 import { Container } from './styles';
 
 const ValidationFieldIndicator = ({ message }) => {
   return (
     <Container>
       <ActivityIndicator style={{ marginRight: 2 }} />
-      <Text>{message}</Text>
+      <Text style={{ color: CORES.PRETO }}>{message}</Text>
     </Container>
   );
 };
