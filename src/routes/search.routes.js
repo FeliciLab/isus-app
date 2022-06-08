2a0a5457dea2ecc0356bf3284f07f16cc21ee053
ericsonmoreira <moreira.ericson@gmail.com>
2022-06-08 09:30:10 -0300

    🎨 ajustando código

--- src/routes/search.routes.js
+++ src/routes/search.routes.js
@@ -4,23 +4,21 @@ import Buscar from '~/pages/Buscar';
 import BuscarDescription from '~/pages/Buscar/Description';
 import rotas from '~/constantes/rotas';
 
-const SearchStack = createStackNavigator();
+const { Screen, Navigator } = createStackNavigator();
 
-const SearchStackScreen = () => {
-  return (
-    <SearchStack.Navigator>
-      <SearchStack.Screen
-        name={rotas.SEARCH_PAGE}
-        component={Buscar}
-        options={{ headerShown: true }}
-      />
-      <SearchStack.Screen
-        name={rotas.SEARCH_PAGE_DESCRIPTION}
-        component={BuscarDescription}
-        options={{ headerShown: true }}
-      />
-    </SearchStack.Navigator>
-  );
-};
+const SearchStackScreen = () => (
+  <Navigator>
+    <Screen
+      name={rotas.SEARCH_PAGE}
+      component={Buscar}
+      options={{ headerShown: true }}
+    />
+    <Screen
+      name={rotas.SEARCH_PAGE_DESCRIPTION}
+      component={BuscarDescription}
+      options={{ headerShown: true }}
+    />
+  </Navigator>
+);
 
 export default SearchStackScreen;
