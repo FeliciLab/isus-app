433de8cf509571b78a93e6ace5744bba1e990e04
ericsonmoreira <moreira.ericson@gmail.com>
2022-08-23 16:16:06 -0300

    ⚡️ otimizando and clean code

--- src/pages/Home/Banners/Banner/index.js
+++ src/pages/Home/Banners/Banner/index.js
@@ -1,6 +1,6 @@
 import { useNetInfo } from '@react-native-community/netinfo';
 import { useNavigation } from '@react-navigation/native';
-import React from 'react';
+import React, { useCallback } from 'react';
 import rotas from '~/constantes/rotas';
 import useAnalytics from '~/hooks/useAnalytics';
 import BannerImagem from '../BannerImagem';
@@ -21,33 +21,36 @@ const Banner = ({ data, ...rest }) => {
 
   const navigation = useNavigation();
 
-  const handleOnPress = () => {
+  const handleOnPress = useCallback(() => {
     analyticsData(labelAnalytics, 'Click', 'Home');
 
-    // Para quando deve abrir uma rota interna
-    if (tipo === 'rota') {
-      // Para quando a rota informada não existe
-      if (!rotas[valor]) {
-        return navigation.navigate(rotas.NOT_FOUND);
-      }
+    const hrandler = {
+      rota: () => {
+        // Para quando a rota informada não existe
+        if (!rotas[valor]) {
+          return navigation.navigate(rotas.NOT_FOUND);
+        }
 
-      return navigation.navigate(valor);
-    }
+        return navigation.navigate(valor);
+      },
+      webview: () => {
+        if (isConnected) {
+          return navigation.navigate(rotas.WEBVIEW_PAGE, {
+            title: titulo,
+            url: valor,
+          });
+        } else {
+          return navigation.navigate(rotas.SEM_CONEXAO, {
+            componente: 'webview',
+            title: titulo,
+            url: valor,
+          });
+        }
+      },
+    };
 
-    // Para quando for tentar abrir uma página na web
-    if (isConnected) {
-      return navigation.navigate(rotas.WEBVIEW_PAGE, {
-        title: titulo,
-        url: valor,
-      });
-    } else {
-      return navigation.navigate(rotas.SEM_CONEXAO, {
-        componente: 'webview',
-        title: titulo,
-        url: valor,
-      });
-    }
-  };
+    hrandler[tipo] ? hrandler[tipo]() : hrandler.webview();
+  }, [isConnected]);
 
   return (
     <Container onPress={handleOnPress} {...rest}>
