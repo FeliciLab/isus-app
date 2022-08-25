8f5e379f0b540540f512f94c58f9831c9ea57a61
Ericson Moreira <moreira.ericson@gmail.com>
2022-08-25 12:01:03 -0300

    💄 ajustando layout

--- src/pages/QualiQuiz/index.js
+++ src/pages/QualiQuiz/index.js
@@ -1,9 +1,9 @@
 import { useFocusEffect, useNavigation } from '@react-navigation/native';
-import React, { useEffect, useLayoutEffect, useRef } from 'react';
-import { BackHandler, View } from 'react-native';
+import React, { useEffect, useRef } from 'react';
+import { BackHandler, StyleSheet, Text, View } from 'react-native';
 import { Config } from 'react-native-config';
-import IsusSvg from '~/assets/icons/isus_hor.svg';
-import { cabecalhoVoltar } from '~/components/layoutEffect/cabecalhoLayout';
+import QualiQuizLogo from '~/assets/images/qualiquiz/logo.svg';
+import BarraDeStatus from '~/components/BarraDeStatus';
 import { CORES } from '~/constantes/estiloBase';
 import rotas from '~/constantes/rotas';
 import useAutenticacao from '~/hooks/useAutenticacao';
@@ -55,24 +55,39 @@ export default function QualiQuiz({ navigation }) {
     handleEffect();
   });
 
-  useLayoutEffect(
-    () =>
-      cabecalhoVoltar({
-        title: 'QualiQuiz',
-        navegador: navigation,
-        cor: 'verde',
-      }),
-    [],
-  );
-
   return (
-    <View
-      style={{
-        flexDirection: 'column',
-        paddingTop: '20%',
-        alignItems: 'center',
-      }}>
-      <IsusSvg height={250} width={250} />
+    <View style={styles.container}>
+      <BarraDeStatus
+        backgroundColor={CORES.QUALIQUIZ}
+        barStyle="light-content"
+      />
+      <View style={styles.content}>
+        <QualiQuizLogo height={250} width={250} />
+        <Text style={styles.textContent}>
+          Carregando, aguarde um instante...
+        </Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: CORES.QUALIQUIZ_LIGTH,
+  },
+  content: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textContent: {
+    textAlign: 'center',
+    color: CORES.BRANCO,
+    lineHeight: 19,
+    fontWeight: '500',
+    fontSize: 16.5,
+    marginTop: 38,
+  },
+});
