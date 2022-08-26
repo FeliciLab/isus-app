813776e2553e47ef4b72e5bf23f6703192559f98
Ericson Moreira <moreira.ericson@gmail.com>
2022-08-26 10:13:56 -0300

    🚧 ajustando o schema de validação de campos

--- src/pages/FaleConosco/DuvidasResidenciasFrom/schema.js
+++ src/pages/FaleConosco/DuvidasResidenciasFrom/schema.js
@@ -1,9 +1,13 @@
 import * as yup from 'yup';
 
 const schema = yup.object({
-  descricao: yup.string().required('Campo obrigatório'),
-  unidadeDeSaude: yup.string().required('Campo obrigatório'),
-  email: yup.string().email('Email inválido'),
+  assuntoSelectedId: yup.string().required('Campo obrigatório'),
+  mensagem: yup.string().required('Campo obrigatório'),
+  curso: yup.string().required('Campo obrigatório'),
+  email: yup
+    .string()
+    .required('Campo obrigatório')
+    .email('Digite um formato válido de e-mail'),
 });
 
 export default schema;
